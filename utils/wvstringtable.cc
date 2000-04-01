f3e8a1d5129f0ef40697073e3328d8a9b64e231d
apenwarr <apenwarr@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2000-04-01 10:40:47 +0000

    The user setup webconfig page is now pretty passable.  At least, it seems to do everything it needs to do.  I want the "team" edit window to eventually do the opposite of the "user" edit window (users should show the list of possible teams to join; teams should show the list of possible users/teams that can be members).  For now, I'll be lazy and just have everyone show which teams they're on.  You can accomplish everything that way, it's just not as convenient.
    Anonymous ftp creates a team called "ftp" automatically to hold the account.
    Only lightly tested.
    
    
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@198 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ utils/wvstringtable.cc
@@ -0,0 +1,39 @@
+/*
+ * Worldvisions Weaver Software:
+ *   Copyright (C) 1997-2000 Net Integration Technologies, Inc.
+ *
+ * Some helper functions for WvStringTable.
+ */
+#include "wvstringtable.h"
+#include "strutils.h"
+
+
+WvString WvStringTable::join(const char *joinchars = " ")
+{
+    WvStringTable::Iter s(*this);
+    size_t totlen;
+    WvString total;
+    char *te;
+    int x;
+    
+    totlen = 1;
+    for (s.rewind(); s.next(); )
+	totlen += strlen(s()) + strlen(joinchars);
+    
+    total.setsize(totlen);
+    te = total.edit();
+    
+    te[0] = 0;
+    x = 0;
+    for (s.rewind(); s.next(); )
+    {
+	if (x++)
+	    strcat(te, joinchars);
+	strcat(te, s());
+    }
+    
+    if (te[0])
+	trim_string(te);
+    
+    return total;
+}
--- utils/wvstringtable.h
+++ utils/wvstringtable.h
@@ -6,13 +6,14 @@
  * not be defined most of the time.  Include this file if you need it.
  *
  */
-
 #ifndef __WVSTRINGTABLE_H
 #define __WVSTRINGTABLE_H
 
 #include "wvstring.h"
 #include "wvhashtable.h"
 
-DeclareWvTable(WvString);
+DeclareWvTable2(WvString,
+		WvString join(const char *joinchars = " ");
+		);
 
 #endif // __WVSTRINGTABLE_H
