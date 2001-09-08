b36aa2dcac1954e020ceb00993fda5df5ffa0b16
apenwarr <apenwarr@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2001-09-08 22:34:58 +0000

    r3_5: added WvUnixAddr, WvUnixConn, and WvUnixListener classes for Unix domain sockets.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r3_5@279 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ ipstreams/wvunixsocket.cc
@@ -0,0 +1,172 @@
+/*
+ * Worldvisions Weaver Software:
+ *   Copyright (C) 1997-2001 Net Integration Technologies, Inc.
+ * 
+ * WvStream-based Unix domain socket connection class.  See wvunixsocket.h.
+ */
+#include "wvstreamlist.h"
+#include "wvunixsocket.h"
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <errno.h>
+
+
+WvUnixConn::WvUnixConn(int _fd, const WvUnixAddr &_addr)
+    : addr(_addr)
+{
+    fd = _fd;
+    
+    // already connected...
+}
+
+
+WvUnixConn::WvUnixConn(const WvUnixAddr &_addr)
+    : addr(_addr)
+{
+    sockaddr *sa;
+    
+    fd = socket(PF_UNIX, SOCK_STREAM, 0);
+    if (fd < 0)
+    {
+	seterr(errno);
+	return;
+    }
+    
+    sa = addr.sockaddr();
+    if (connect(fd, sa, addr.sockaddr_len()) < 0)
+    {
+	seterr(errno);
+	delete sa;
+	return;
+    }
+    
+    delete sa;
+    
+    // all is well and we're connected.  Make it non-blocking 
+    // and close-on-exec.
+    fcntl(fd, F_SETFD, 1);
+    fcntl(fd, F_SETFL, O_RDWR|O_NONBLOCK);
+}
+
+
+WvUnixConn::~WvUnixConn()
+{
+    // we don't want to delete the socket file here; that's a job for the
+    // listener class.
+    
+    // close the socket
+    close();
+}
+
+
+const WvUnixAddr *WvUnixConn::src() const
+{
+    return &addr;
+}
+
+
+WvUnixListener::WvUnixListener(const WvUnixAddr &_addr, int create_mode)
+	: addr(_addr)
+{
+    mode_t oldmask;
+    
+    auto_list = NULL;
+    auto_callback = NULL;
+    auto_userdata = NULL;
+    bound_okay = false;
+    
+    sockaddr *sa = addr.sockaddr();
+    
+    oldmask = umask((~create_mode) & 0x0777);
+    
+    fd = socket(PF_UNIX, SOCK_STREAM, 0);
+    if (fd < 0
+	|| fcntl(fd, F_SETFD, 1)
+	|| bind(fd, sa, addr.sockaddr_len())
+	|| listen(fd, 5))
+    {
+	seterr(errno);
+    }
+    else
+	bound_okay = true;
+    
+    delete sa;
+    umask(oldmask);
+}
+
+
+WvUnixListener::~WvUnixListener()
+{
+    close();
+}
+
+
+void WvUnixListener::close()
+{
+    // delete the socket _before_ closing it.  Unix will keep
+    // existing connections around anyway (if any), but if it's idle, then
+    // we never have an existing not-in-use socket inode.
+    if (bound_okay)
+    {
+	WvString filename(addr);
+	::unlink(filename);
+    }
+    
+    WvStream::close();
+}
+
+
+WvUnixConn *WvUnixListener::accept()
+{
+    struct sockaddr_un sun;
+    size_t len = sizeof(sun);
+    int newfd;
+    WvUnixConn *ret;
+
+    newfd = ::accept(fd, (struct sockaddr *)&sun, &len);
+    ret = new WvUnixConn(newfd, addr);
+    return ret;
+}
+
+
+void WvUnixListener::auto_accept(WvStreamList *list,
+				 Callback *callfunc, void *userdata)
+{
+    auto_list = list;
+    auto_callback = callfunc;
+    auto_userdata = userdata;
+    setcallback(accept_callback, this);
+}
+
+
+void WvUnixListener::accept_callback(WvStream &, void *userdata)
+{
+    WvUnixListener &l = *(WvUnixListener *)userdata;
+
+    WvUnixConn *connection = l.accept();
+    connection->setcallback(l.auto_callback, l.auto_userdata);
+    l.auto_list->append(connection, true);
+}
+
+
+size_t WvUnixListener::uread(void *, size_t)
+{
+    return 0;
+}
+
+
+size_t WvUnixListener::uwrite(const void *, size_t)
+{
+    return 0;
+}
+
+
+const WvUnixAddr *WvUnixListener::src() const
+{
+    return &addr;
+}
+
