84a1f48542ff60026ce61efe93698c1e3806076a
apenwarr <apenwarr@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2001-10-14 03:59:21 +0000

    Updated LDAP server to use a more standards-compliant layout.  It also now works with pam_ldap, so you can set up a debian system to authenticate against a Net Integrator's LDAP server.  Ooh.
    Also made it so you don't need to specify any base dn when you configure an
    ldap client.  Instead, the ldap server sends a referral to the right one.
    At least, you can say goodbye to "o=net-itech.com".  That base dn was
    totally nonstandard anyway - the correct one is "dc=net-itech,dc=com", which
    we now use... but you don't have to type it anywhere so that's okay.
    
    Also made NFS send a SIGHUP to mountd when it reconfigures /etc/exports.
    
    
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@293 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ utils/strcrypt.cc
@@ -0,0 +1,22 @@
+#include "strutils.h"
+#include <crypt.h>
+#include <stdlib.h>
+
+WvString passwd_crypt(const char *str)
+{
+    static char saltchars[] = 
+	"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./";
+    char salt[3], *result;
+    
+    salt[0] = saltchars[random() % (sizeof(saltchars) - 1)];
+    salt[1] = saltchars[random() % (sizeof(saltchars) - 1)];
+    salt[2] = 0;
+	
+    result = crypt(str, salt);
+    if (!result)
+	return "*";
+
+    WvString s(result);
+    s.unique();
+    return s;
+}
--- utils/strutils.h
+++ utils/strutils.h
@@ -11,17 +11,18 @@
 
 #include "wvstring.h"
 
-extern char *terminate_string( char *string, char c );
-extern char *trim_string( char *string );
-extern void replace_char( void *string, char c1, char c2, int length );
-extern char *strlwr( char * string );
-extern bool is_word( char * string );
-extern WvString hexdump_buffer( unsigned char *buf, size_t len );
-extern bool isnewline( char c );
-extern void hexify( char *obuf, unsigned char *ibuf, size_t len );
-extern void unhexify( unsigned char *obuf, char *ibuf );
-extern WvString web_unescape(const char *str);
-extern WvString rfc822_date(time_t when=-1);
+char *terminate_string(char *string, char c);
+char *trim_string(char *string);
+void replace_char(void *string, char c1, char c2, int length);
+char *strlwr(char * string);
+bool is_word(char * string);
+WvString hexdump_buffer(unsigned char *buf, size_t len);
+bool isnewline(char c);
+void hexify(char *obuf, unsigned char *ibuf, size_t len);
+void unhexify(unsigned char *obuf, char *ibuf);
+WvString web_unescape(const char *str);
+WvString rfc822_date(time_t when=-1);
+WvString passwd_crypt(const char *str);
 
 
 #endif // __STRUTILS_H
