cf2a427fff28f9117e04c95e8617cc903a6c3d13
apenwarr <apenwarr@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2002-02-23 00:05:50 +0000

    WvHConf now supports inheritence, I think.  Only lightly tested, but it seems to work.  It might not support multi-level inheritence (A inherits from B, but if B doesn't contain it, we fall through to C).  But then again, it might.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@471 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- hconf/hconftest.cc
+++ hconf/hconftest.cc
@@ -31,10 +31,11 @@ public:
 class WvHConf
 {
 public:
-    WvHConf *parent;
-    WvString name;
+    WvHConf *parent;       // the 'parent' of this subtree
+    WvString name;         // any node contains 'name = value' data
     WvString value;
-    WvHConfDict *subkeys;
+    WvHConf *defaults;     // a tree possibly containing default values
+    WvHConfDict *children; // list of all child nodes of this node (subkeys)
     
     WvHConf();
     WvHConf(WvHConf *_parent, const WvString &_name);
@@ -47,12 +48,18 @@ public:
     WvHConf *find_make(const WvHConfKey &key);
     WvHConf &operator[](const WvHConfKey &key) { return *find_make(key); }
     
+    WvHConf *find_default(WvHConfKey *_k = NULL) const;
+    
     WvString &get(const WvHConfKey &key)
         { return find_make(key)->value; }
     
     void set(const WvHConfKey &key, const WvString &value)
         { find_make(key)->value = value; }
+    
     WvString &operator= (const WvString &s) { return value = s; }
+    
+    const WvString& printable() const;
+    operator const WvString& () { return printable(); }
 };
 
 DeclareWvDict(WvHConf, WvString, name);
@@ -117,7 +124,8 @@ WvHConf::WvHConf()
     : name("")
 {
     parent = NULL;
-    subkeys = NULL;
+    defaults = NULL;
+    children = NULL;
 }
 
 
@@ -125,14 +133,15 @@ WvHConf::WvHConf(WvHConf *_parent, const WvString &_name)
     : name(_name)
 {
     parent = _parent;
-    subkeys = NULL;
+    defaults = NULL;
+    children = NULL;
 }
 
 
 WvHConf::~WvHConf()
 {
-    if (subkeys)
-	delete subkeys;
+    if (children)
+	delete children;
 }
 
 
@@ -166,51 +175,112 @@ WvHConfKey WvHConf::full_key() const
 
 
 // find a key in the subtree.  If it doesn't already exist, return NULL.
-WvHConf *WvHConf::find(const WvHConfKey &subkey)
+WvHConf *WvHConf::find(const WvHConfKey &key)
 {
-    if (subkey.isempty())
+    if (key.isempty())
 	return this;
-    if (!subkeys)
+    if (!children)
 	return NULL;
     
-    WvHConf *h = (*subkeys)[*subkey.first()];
+    WvHConf *h = (*children)[*key.first()];
     if (!h)
 	return NULL;
     else
-	return h->find(subkey.skip(1));
+	return h->find(key.skip(1));
 }
 
 
 // find a key in the subtree.  If it doesn't already exist, create it.
-WvHConf *WvHConf::find_make(const WvHConfKey &subkey)
+WvHConf *WvHConf::find_make(const WvHConfKey &key)
 {
-    if (subkey.isempty())
+    if (key.isempty())
 	return this;
-    if (!subkeys)
-	subkeys = new WvHConfDict(10);
+    if (!children)
+	children = new WvHConfDict(10);
     
-    WvHConf *h = (*subkeys)[*subkey.first()];
+    WvHConf *h = (*children)[*key.first()];
     
     if (h)
-	return h->find_make(subkey.skip(1));
+	return h->find_make(key.skip(1));
     else
     {
 	// create a subkey, then call it recursively.
-	WvHConf *child = new WvHConf(this, *subkey.first());
-	subkeys->add(child, true);
-	return child->find_make(subkey.skip(1));
+	WvHConf *child = new WvHConf(this, *key.first());
+	children->add(child, true);
+	return child->find_make(key.skip(1));
     }
+}
+
+
+// find a key that contains the default value for this key, regardless of
+// whether this key _needs_ a default value or not.  (If !!value, we no longer
+// need a default, but this function still can return non-NULL.)
+// 
+// If there's no available default value for this key, return NULL.
+// 
+WvHConf *WvHConf::find_default(WvHConfKey *_k) const
+{
+    WvHConfKey tmp_key;
+    WvHConfKey &k = (_k ? *_k : tmp_key);
+    WvHConf *def;
+    
+    //wvcon->print("find_default for '%s'\n", full_key());
+	
+    if (defaults)
+    {
+	//wvcon->print("  find key '%s' in '%s'\n", k, defaults->full_key());
+	def = defaults->find(k);
+	if (def)
+	    return def;
+    }
+    
+    if (parent)
+    {
+	// go up a level and try again
+	WvString s1(name);
+	k.prepend(&s1, false);
+	def = parent->find_default(&k);
+	k.unlink_first();
+	if (def)
+	    return def;
+	
+	// try with a wildcard instead
+	WvString s2("*");
+	k.prepend(&s2, false);
+	def = parent->find_default(&k);
+	k.unlink_first();
+	if (def)
+	    return def;
+    }
+    
+    return NULL;
+}
+
+
+const WvString &WvHConf::printable() const
+{
+    WvHConf *def;
+    
+    if (!!value)
+	return value;
+    
+    def = find_default();
+    if (def)
+	return def->printable();
+    
+    // no default found: return the value (basically NULL) anyway
+    return value;
 }
 
 
 void dumpcfg(WvHConf &cfg, WvStream &s)
 {
     if (!!cfg.value)
-	s.print("%s = %s\n", cfg.full_key(), cfg.value);
+	s.print("\t%s = %s\n", cfg.full_key(), cfg.value);
 
-    if (cfg.subkeys)
+    if (cfg.children)
     {
-	WvHConfDict::Iter i(*cfg.subkeys);
+	WvHConfDict::Iter i(*cfg.children);
 	for (i.rewind(); i.next(); )
 	    dumpcfg(*i, s);
     }
@@ -220,21 +290,55 @@ void dumpcfg(WvHConf &cfg, WvStream &s)
 int main()
 {
     WvLog log("hconftest", WvLog::Info);
-    WvHConf cfg;
     
-    WvHConfKey key("/a/b/c/d/e/f/ghij////k/l/m");
-    WvHConfKey key2(key), key3(key, 5), key4(key, 900);
+    {
+	log("-- Key test begins\n");
+	
+	WvHConfKey key("/a/b/c/d/e/f/ghij////k/l/m");
+	WvHConfKey key2(key), key3(key, 5), key4(key, 900);
+	log("key : %s\nkey2: %s\nkey3: %s\nkey4: %s\n", key, key2, key3, key4);
+    }
     
-    log("key : %s\nkey2: %s\nkey3: %s\nkey4: %s\n", key, key2, key3, key4);
+    {
+	log("-- Basic config test begins\n");
+	
+	WvHConf cfg;
+	cfg.set("/foo/blah/weasels", "chickens");
+	
+	cfg["foo"]["pah"]["meatballs"] = 6;
+	
+	WvHConf &x = cfg["snort/fish/munchkins"];
+	x.set("big/bad/weasels", 7);
+	
+	log("Config dump:\n");
+	dumpcfg(cfg, *wvcon);
+    }
     
-    cfg.set("/foo/blah/weasels", "chickens");
-
-    cfg["foo"]["pah"]["meatballs"] = 6;
-
-    WvHConf &x = cfg["snort/fish/munchkins"];
-    x.set("big/bad/weasels", 7);
-    
-    dumpcfg(cfg, *wvcon);
+    {
+	log("-- Inheritence test begins\n");
+	
+	WvHConf cfg, *h;
+	
+	cfg.set("/default/users/*/comment", "defuser comment");
+	cfg.set("/default/users/bob/comment", "defbob comment");
+	
+	log("Old comment settings are: %s/%s\n",
+	    cfg["/users/randomperson/comment"], cfg["/users/bob/comment"]);
+	
+	cfg["/users"].defaults = &cfg["/default/users"];
+	
+	h = cfg["/users/randomperson/comment"].find_default();
+	log("Default for randomperson: '%s'\n", h ? *h : WvString("NONE"));
+	
+	h = cfg["/users/bob/comment"].find_default();
+	log("Default for bob: '%s'\n", h ? *h : WvString("NONE"));
+	
+	log("New comment settings are: %s/%s\n",
+	    cfg["/users/noperson/comment"], cfg["/users/bob/comment"]);
+	
+	log("Config dump 2:\n");
+	dumpcfg(cfg, *wvcon);
+    }
     
     return 0;
 }
