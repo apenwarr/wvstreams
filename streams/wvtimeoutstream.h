9f05251c79f303088d0b015b27226e20ba251508
pphaneuf <pphaneuf@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2002-04-10 16:26:49 +0000

    Made WvTimeoutStream threadsafe.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@601 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- streams/wvtimeoutstream.h
+++ streams/wvtimeoutstream.h
@@ -33,7 +33,7 @@ public:
     }
 private:
     static int getmsec() {
-	static struct timeval tv;
+	struct timeval tv;
 	gettimeofday(&tv, NULL);
 	return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
     }
