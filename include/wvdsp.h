a3c01c51a5e0cc6dd3204909ca54f2ad70f3c1b6
ppatters <ppatters@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2002-09-30 18:34:08 +0000

    A couple of documentation updates for wvdsp, and some public vs. private fixes...
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@1103 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- include/wvdsp.h
+++ include/wvdsp.h
@@ -12,30 +12,66 @@
 #include "wvloopback.h"
 #include "wvlog.h"
 
-
+/**
+ * Class to access the /dev/dsp device in a way that's sane enough for
+ * full-duplex access. This is still rather heavily under construction at
+ * this time, so please don't count on any side-effects to make anything
+ * built with this class to work.
+ */
 class WvDsp : public WvStream
 {
 public:
+
+    /**
+     * Construct a /dev/dsp accessor object
+     * msec_latency = number of milliseconds of latency that are permissible
+     * srate = sampling rate ( 44, 22, 11, or 8 Hz )
+     * bits = bits per sample ( 8 or 16 )
+     * stereo = should this be a stereo stream?
+     * readable/writeable = should this stream be readable and/or writeable
+     */
     WvDsp(int msec_latency, int srate, int bits, bool stereo,
 	  bool readable, bool writable);
     virtual ~WvDsp();
-    
+
+    /**
+     * Reimplemented from @ref WvStreams for internal reasons
+     */
     virtual bool pre_select(SelectInfo &si);
+
+    /**
+     * Reimplemented from @ref WvStreams for internal reasons
+     */
     virtual bool post_select(SelectInfo &si);
-    
+
+    /**
+     * Reimplemented from @ref WvStreams for internal reasons
+     */
     virtual size_t uread(void *buf, size_t len);
+
+    /**
+     * Reimplemented from @ref WvStreams for internal reasons
+     */
     virtual size_t uwrite(const void *buf, size_t len);
 
+
+    /**
+     * Reimplemented from @ref WvStreams for internal reasons
+     */
     virtual bool isok() const;
+
+    /**
+     * Reimplemented from @ref WvStreams for internal reasons
+     */
     virtual void close();
 
-public:
-    bool setioctl(int ctl, int param);
-    void realtime();
-    void subproc(bool reading, bool writing);
-    
     size_t ispace();
     size_t ospace();
+    void realtime();
+
+private:
+    bool setioctl(int ctl, int param);
+    void subproc(bool reading, bool writing);
     
     size_t do_uread(void *buf, size_t len);
     size_t do_uwrite(const void *buf, size_t len);
