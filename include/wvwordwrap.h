a74b70fe30d98d3d29043221aff2ffdeb474c316
jbrown <jbrown@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2002-10-18 01:50:26 +0000

    Added first draft of WvWordWrap.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@1207 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ include/wvwordwrap.h
@@ -0,0 +1,41 @@
+/*
+ * Worldvisions Weaver Software:
+ *   Copyright (C) 1997-2002 Net Integration Technologies, Inc.
+ *
+ * A very simple word wrapping encoder.
+ */
+#ifndef __WVWORDWRAP_H
+#define __WVWORDWRAP_H
+ 
+#include "wvencoder.h"
+ 
+/**
+ * Trivial word wrapper.
+ * Recognizes '\n' in input stream as end of line.
+ * Words are output until no more will fit, in which case a newline
+ * is output and the word is presented on the next line.  Wrapped
+ * word delimiter characters are discarded such that a wrapped word
+ * will always be placed at the beginning of a line.
+ */
+class WvWordWrapEncoder : public WvEncoder
+{
+    const int maxwidth;
+    char *line;
+    int width;     // current visual position
+    int curindex;  // current index in line array
+    int wordindex; // index of beginning of word in line array
+    bool inword;   // if true, we're in a word
+
+public:
+    WvWordWrapEncoder(int maxwidth);
+    virtual ~WvWordWrapEncoder();
+
+protected:
+    // on flush, outputs a partial line with remaining chars
+    virtual bool _encode(WvBuffer &inbuf, WvBuffer &outbuf, bool flush);
+
+private:
+    void flushline(WvBuffer &outbuf);
+};
+
+#endif // __WVWORDWRAP_H
