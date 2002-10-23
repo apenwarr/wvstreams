02e4e8f66201065103b81b9761a1aec5d03f1a4f
pphaneuf <pphaneuf@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2002-10-23 23:03:22 +0000

    Removed the non-working "nimon" WvQt test until it is fixed.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@1240 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- qt/tests/vars.mk
+++ qt/tests/vars.mk
@@ -1,15 +1,5 @@
 
-ifdef INCLUDE_QT
+ifneq ("$(with_qt)", "no")
 TESTS+=$(patsubst %.cc,%,$(wildcard qt/tests/*.cc))
 endif
 
-%.moc: %.h
-	@echo -n "Creating MOC file from $< ..."
-	@moc $< -o $@
-	@echo "Done."
-        
-qt/tests/nimon.o: qt/tests/nimon.moc
-
-qt/tests/nimon-LIBS:=libwvqt.a -lqt -lkdeui -lkio
-
-CPPFLAGS += -I/usr/include/kde
