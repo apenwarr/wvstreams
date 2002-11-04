b042e1b13697bb1ca159b6a5a285a1321c118d3b
jbrown <jbrown@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2002-11-04 21:37:18 +0000

    More documentation improvements.  Doxygen'd WvEncoder is actually usable!
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@1326 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- include/wvencoder.h
+++ include/wvencoder.h
@@ -1,13 +1,8 @@
 /*
  * Worldvisions Weaver Software:
  *   Copyright (C) 1997-2002 Net Integration Technologies, Inc.
- * 
- * A top-level data encoder class.  You put data in, and when it comes out
- * it's encoded.  It might be a different size or not, and future data might
- * or might not depend on previous data, depending on the encoder type.
- * 
- * Decoders are also derived from WvEncoder, since they're
- * really just encoders too.
+ *
+ * A top-level data encoder class.
  */
 #ifndef __WVENCODER_H
 #define __WVENCODER_H
@@ -18,6 +13,40 @@
 
 /**
  * The base encoder class.
+ * <p>
+ * Encoders read data from an input buffer, transform it in some
+ * way, then write the results to an output buffer.  The resulting
+ * data may be of a different size or data type, and may or may
+ * not depend on previous data.
+ * </p><p>
+ * Encoders may or may not possess the following characteristics:
+ * <ul>
+ * <li>Statefulness: encoding of successive input elements may
+ *     depend on previous one</li>
+ * <li>Error states: encoding may enter an error state indicated
+ *     by <code>isok() == false</code> due to problems detected
+ *     in the input, or by the manner in which the encoder has
+ *     been user</li>
+ * <li>Minimum input block size: data will not be drawn from the
+ *     input buffer until enough is available or the encoder
+ *     is flushed</li>
+ * <li>Minimum output block size: data will not be written to the
+ *     output buffer until enough free space is available</li>
+ * <li>Synchronization boundaries: data is process or generated
+ *     in chunks which can be manipulated independently of any
+ *     others, in which case flush() may cause the encoder to
+ *     produce such a boundary in its output</li>
+ * <li>Recognition of end-of-data mark: a special sequence marks
+ *     the end of input, after which the encoder transitions to
+ *     <code>isfinished() == true</code></li>
+ * <li>Generation of end-of-data mark: a special sequence marks
+ *     the end of output when the encoder transitions to
+ *     <code>isfinished() == true</code>, usually by an explicit
+ *     call to finish()</li>
+ * <li>Reset support: the encoder may be reset to its initial
+ *     state and thereby recycled at minimum cost</li>
+ * </ul>
+ * </p>
  */
 class WvEncoder
 {
@@ -37,23 +66,23 @@ public:
     virtual ~WvEncoder();
     
     /**
-     * Returns true if the encoder can encode more data.
-     *
+     * Returns true if the encoder has not encountered an error.
+     * <p>
      * This should only be used to record permanent failures.
      * Transient errors (eg. bad block, but recoverable) should be
      * detected in a different fashion.
-     *
+     * </p>
      * @return true if the encoder is ok
      */
     bool isok() const
         { return okay && _isok(); }
 
     /**
-     * Returns true if the encoder can no longer process data.
-     *
+     * Returns true if the encoder can no longer encode data.
+     * <p>
      * This will be set when the encoder detects and end-of-data
      * mark in its input, or when finish() is called.
-     *
+     * </p>
      * @return true if the encoder is finished
      */
     bool isfinished() const
@@ -69,7 +98,7 @@ public:
     /**
      * Reads data from the input buffer, encodes it, and writes the result
      * to the output buffer.
-     *
+     * <p>
      * If flush == true, the input buffer will be drained and the output
      * buffer will contain all of the encoded data including any that
      * might have been buffered internally from previous calls.  Thus it
@@ -78,13 +107,13 @@ public:
      * buffer could not be fully drained because there was insufficient
      * data, this function returns false and leaves the remaining unflushed
      * data in the buffer.
-     *
+     * </p><p>
      * If flush == false, the encoder will read and encode as much data
      * as possible (or as it convenient) from the input buffer and store
      * the results in the output buffer.  Partial results may be buffered
      * internally by the encoder to be written to the output buffer later
      * when the encoder is flushed.
-     *
+     * </p><p>
      * If finish = true, the encode() will be followed up by a call to
      * finish().  The return values will be ANDed together to yield the
      * final result.  Most useful when flush is also true.
@@ -92,14 +121,14 @@ public:
      * If a permanent error occurs, then isok() will return false, this
      * function will return false and the input buffer will be left in an
      * undefined state.
-     *
+     * </p><p>
      * If a recoverable error occurs, the encoder should discard the
      * problematic data from the input buffer and return false from this
      * function, but isok() will remain true.
-     *
+     * </p><p>
      * A stream might become isfinished() == true if an encoder-
      * specific end-of-data marker was detected in the input.
-     *
+     * </p>
      * @param inbuf the input buffer
      * @param outbuf the output buffer
      * @param flush if true, flushes the encoder
@@ -124,20 +153,17 @@ public:
 
     /**
      * Tells the encoder that NO MORE DATA will ever be encoded.
-     *
-     * It should flush out any and all internally buffered data
-     * and write out whatever end-of-data marking it needs before
-     * returning.
-     *
+     * <p>
+     * The encoder will flush out any internally buffered data
+     * and write out whatever end-of-data marking it needs to the
+     * supplied output buffer before returning.
+     * </p><p>
      * Clients should invoke flush() on the input buffer before
      * finish() if the input buffer was not yet empty.
-     *
+     * </p><p>
      * It is safe to call this function multiple times.
      * The implementation will simply return isok() and do nothing else.
-     *
-     * Returns true on success or false if an error occurs.
-     * Returns isok() if the stream had previously been finished.
-     *
+     * </p>
      * @param outbuf the output buffer
      * @return true on success
      * @see _finish for the actual implementation
@@ -147,14 +173,13 @@ public:
     /**
      * Asks an encoder to reset itself to its initial state at
      * creation time, if supported.
-     *
+     * <p>
      * This function may be called at any time, even if
      * isok() == false, or isfinished() == true.
-     *
-     * Returns true if the encoder has successfully been reset.
+     * </p><p>
      * If the behaviour is not supported or an error occurs,
-     * then afterwards isok() == false.
-     *
+     * then false is returned and afterwards isok() == false.
+     * </p>
      * @return true on success
      * @see _reset for the actual implementation
      */
@@ -179,9 +204,9 @@ public:
         
     /**
      * Flushes data through the encoder from a string to a string.
-     *
+     * <p>
      * The output data is appended to the target string.
-     *
+     * </p>
      * @param instr the input string
      * @param outstr the output string
      * @param finish if true, calls finish() on success
@@ -192,9 +217,9 @@ public:
 
     /**
      * Encodes data from a buffer to a string.
-     *
+     * <p>
      * The output data is appended to the target string.
-     *
+     * </p>
      * @param inbuf the input buffer
      * @param outstr the output string
      * @param flush if true, flushes the encoder
@@ -206,9 +231,9 @@ public:
 
     /**
      * Flushes data through the encoder from a buffer to a string.
-     *
+     * <p>
      * The output data is appended to the target string.
-     *
+     * </p>
      * @param inbuf the input buffer
      * @param outstr the output string
      * @param finish if true, calls finish() on success
@@ -262,13 +287,13 @@ public:
         
     /**
      * Flushes data through the encoder from memory to memory.
-     *
+     * <p>
      * The <code>outlen</code> parameter specifies by reference
      * the length of the output buffer.  It is updated in place to
      * reflect the number of bytes copied to the output buffer.
      * If the buffer was too small to hold the data, the overflow
      * bytes will be discarded and false will be returned.
-     *
+     * </p>
      * @param inmem the input data pointer
      * @param inlen the input data length
      * @param outmem the output data pointer
@@ -281,13 +306,13 @@ public:
         
     /**
      * Encodes data from a buffer to memory.
-     *
+     * <p>
      * The <code>outlen</code> parameter specifies by reference
      * the length of the output buffer.  It is updated in place to
      * reflect the number of bytes copied to the output buffer.
      * If the buffer was too small to hold the data, the overflow
      * bytes will be discarded and false will be returned.
-     *
+     * </p>
      * @param inmem the input data pointer
      * @param inlen the input data length
      * @param outmem the output data pointer
@@ -301,13 +326,13 @@ public:
         
     /**
      * Flushes data through the encoder from a buffer to memory.
-     *
+     * <p>
      * The <code>outlen</code> parameter specifies by reference
      * the length of the output buffer.  It is updated in place to
      * reflect the number of bytes copied to the output buffer.
      * If the buffer was too small to hold the data, the overflow
      * bytes will be discarded and false will be returned.
-     *
+     * </p>
      * @param inbuf the input buffer
      * @param outmem the output data pointer
      * @param outlen the output data length, by reference
@@ -320,13 +345,13 @@ public:
 
     /**
      * Flushes data through the encoder from a string to memory.
-     *
+     * <p>
      * The <code>outlen</code> parameter specifies by reference
      * the length of the output buffer.  It is updated in place to
      * reflect the number of bytes copied to the output buffer.
      * If the buffer was too small to hold the data, the overflow
      * bytes will be discarded and false will be returned.
-     *
+     * </p>
      * @param instr the input string
      * @param outmem the output data pointer
      * @param outlen the output data length, by reference
@@ -374,10 +399,15 @@ protected:
 protected:
     /**
      * Template method implementation of isok().
+     * <p>
      * Not called if any of the following cases are true:
-     *   okay == false
-     *
+     * <ul>
+     * <li>okay == false</li>
+     * </ul>
+     * </p><p>
      * Most implementations do not need to override this.
+     * </p>
+     * @return true if the encoder is ok
      * @see setnotok
      */
     virtual bool _isok() const
@@ -385,10 +415,15 @@ protected:
 
     /**
      * Template method implementation of isfinished().
+     * <p>
      * Not called if any of the following cases are true:
-     *   finished == true
-     *
+     * <ul>
+     * <li>finished == true</li>
+     * </ul>
+     * </p><p>
      * Most implementations do not need to override this.
+     * </p>
+     * @return true if the encoder is finished
      * @see setfinished
      */
     virtual bool _isfinished() const
@@ -396,11 +431,16 @@ protected:
 
     /**
      * Template method implementation of geterror().
+     * <p>
      * Not called if any of the following cases are true:
-     *   isok() == true
-     *   errstr is not null
-     *
+     * <ul>
+     * <li>isok() == true</li>
+     * <li>errstr is not null</li>
+     * </ul>
+     * </p><p>
      * Most implementations do not need to override this.
+     * </p>
+     * @return the error message, or the null string if _isok() == true
      * @see seterror
      */
     virtual WvString _geterror() const
@@ -408,50 +448,65 @@ protected:
 
     /**
      * Template method implementation of encode().
+     * <p>
      * Not called if any of the following cases are true:
-     *   okay == false       // NOTE: not same as isok()
-     *   finished == true    // NOTE: not same as isfinished()
-     *   in.used() == 0 && flush == false
-     *
+     * <ul>
+     * <li>okay == false</li>
+     * <li>finished == true</li>
+     * <li>in.used() == 0 && flush == false</li>
+     * </ul>
+     * </p><p>
      * All implementations MUST define this.
-     *
+     * </p><p>
      * If you also override _isok() or _isfinished(), note that they
      * will NOT be consulted when determining whether or not to
      * invoke this function.  This allows finer control over the
      * semantics of isok() and isfinished() with respect to encode().
-     *
+     * </p>
+     * @param inbuf the input buffer
+     * @param outbuf the output buffer
+     * @param flush if true, flushes the encoder
+     * @return true on success
      * @see encode
      */
-    virtual bool _encode(WvBuffer &in, WvBuffer &out, bool flush) = 0;
+    virtual bool _encode(WvBuffer &inbuf, WvBuffer &outbuf,
+        bool flush) = 0;
 
     /**
      * Template method implementation of finish().
+     * <p>
      * Not called if any of the following cases are true:
-     *   okay == false       // NOTE: not same as isok()
-     *   finished == true    // NOTE: not same as isfinished()
+     * <ul>
+     * <li>okay == false</li>
+     * <li>finished == true</li>
+     * </ul>
+     * </p><p>
      * The encoder is marked finished AFTER this function exits.
-     *
+     * </p><p>
      * Many implementations do not need to override this.
-     *
+     * </p><p>
      * If you also override _isok() or _isfinished(), note that they
      * will NOT be consulted when determining whether or not to
      * invoke this function.  This allows finer control over the
      * semantics of isok() and isfinished() with respect to finish().
-     *
+     * </p>
+     * @param outbuf the output buffer
+     * @return true on success
      * @see finish
      */
-    virtual bool _finish(WvBuffer &out)
+    virtual bool _finish(WvBuffer &outbuf)
         { return true; }
 
     /**
      * Template method implementation of reset().
-     *
+     * <p>
      * When this method is invoked, the current local state will
      * be okay == true and finished == false.  If false is returned,
      * then okay will be set to false.
-     *
-     * Returns false on error or if the behaviour is not supported.
-     * May also set a detailed error message if an error occurs.
+     * </p><p>
+     * May set a detailed error message if an error occurs.
+     * </p>
+     * @return true on success, false on error or if not supported
      * @see reset
      */
     virtual bool _reset()
@@ -534,56 +589,67 @@ public:
 
 protected:
     /**
-     * Returns true iff all encoders return isok() == true.
-     *
+     * Returns true if the encoder has not encountered an error.
+     * <p>
      * WvEncoderChain is special in that it may transition from
      * isok() == false to isok() == true if the offending encoders
      * are removed from the list.
+     * </p>
+     * @return true iff all encoders return isok() == true
+     * @see WvEncoder::_isok
      */
     virtual bool _isok() const;
     
     /**
-     * Returns false iff all encoders return isfinished() == false.
-     *
+     * Returns true if the encoder can no longer encode data.
+     * <p>
      * WvEncoderChain is special in that it may transition from
      * isfinished() == true to isfinished() == false if the offending
      * encoders are removed from the list, but not if finish() is
      * called.
+     * </p>
+     * @return false iff all encoders return isfinished() == false
      */
     virtual bool _isfinished() const;
 
     /**
-     * Returns the first non-null error message found in the list.
-     *
+     * Returns the error message, if any.
+     * <p>
      * WvEncoderChain is special in that it may transition from
      * !geterror() = false to !geterror() = true if the offending
      * encoders are removed from the list.
+     * </p>
+     * @return the first non-null error message in the chain
      */
     virtual WvString _geterror() const;
     
     /**
      * Passes the data through the entire chain of encoders.
      *
-     * Returns true iff all encoders return true.
+     * @return true iff all encoders return true.
      */
     virtual bool _encode(WvBuffer &in, WvBuffer &out, bool flush);
     
     /**
+     * Finishes the chain of encoders.
+     * <p>
      * Invokes finish() on the first encoder in the chain, then
      * flush() on the second encoder if new data was generated,
      * then finish() on the second encoder, and so on until all
      * encoders have been flushed and finished (assuming the first
      * encoder had already been flushed).
-     *
-     * Returns true iff all encoders return true.
+     * <p>
+     * @return true iff all encoders return true.
      */
     virtual bool _finish(WvBuffer & out);
 
     /**
+     * Resets the chain of encoders.
+     * <p>
      * Resets all of the encoders in the chain and discards any
      * pending buffered input.  Preserves the list of encoders.
-     *
-     * Returns true iff all encoders return true.
+     * </p>
+     * @return true iff all encoders return true.
      */
     virtual bool _reset();
 };
