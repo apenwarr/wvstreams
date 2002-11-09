ecde7428be6acdb20c611e085b1e14436e76fbbd
pphaneuf <pphaneuf@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2002-11-09 06:05:22 +0000

    Renamed testoggvorbis to oggvorbistest, for consistency. Also cleaned up wvstreams/oggvorbis/tests/vars.mk to its minimal expression, to be compatible with an upcoming streamlining.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@1362 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- oggvorbis/tests/vars.mk
+++ oggvorbis/tests/vars.mk
@@ -3,5 +3,6 @@ ifneq ("$(with_oggvorbis)", "no")
 TESTS+=$(patsubst %.cc,%,$(wildcard oggvorbis/tests/*.cc))
 endif
 
-oggvorbis/tests/testoggvorbis-LIBS:=libwvoggvorbis.a \
-    libwvstreams.a libwvutils.a -logg -lvorbis -lvorbisenc
+oggvorbis/tests/oggvorbistest: LDLIBS+=libwvoggvorbis.a
+oggvorbis/tests/oggvorbistest: LDFLAGS+=-logg -lvorbis -lvorbisenc
+
