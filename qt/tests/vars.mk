9eb1a7d4352a490db0fb6906d905ee4e2bc99d6c
pphaneuf <pphaneuf@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2002-11-09 06:11:22 +0000

    Cut, cut, cut, snip, snip, snip in wvstreams/qt/tests/vars.mk...
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@1363 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- qt/tests/vars.mk
+++ qt/tests/vars.mk
@@ -1,12 +1,8 @@
 
 ifneq ("$(with_qt)", "no")
-QT_TESTS = $(patsubst %.cc,%,$(wildcard qt/tests/*.cc))
-TESTS += $(QT_TESTS)
+TESTS+=$(patsubst %.cc,%,$(wildcard qt/tests/*.cc))
 endif
 
-QTT_OBJECTS = $(patsubst %.cc,%.o,$(wildcard qt/tests/*.cc))
-$(QTT_OBJECTS): CPPFLAGS+=-I/usr/include/qt
+qt/tests/qtstringtest: LDLIBS+=libwvqt.a
+qt/tests/qtstringtest: LDFLAGS+=-lqt
 
-$(QT_TESTS): libwvqt.a
-
-qt/tests/qtstringtest-LIBS += -lqt
