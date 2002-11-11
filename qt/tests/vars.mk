081de016abdd6cd4ba34d42bf3550f9a79738cf0
pphaneuf <pphaneuf@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2002-11-11 15:56:18 +0000

    Fixed the WvQt string test.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@1368 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- qt/tests/vars.mk
+++ qt/tests/vars.mk
@@ -3,6 +3,8 @@ ifneq ("$(with_qt)", "no")
 TESTS+=$(patsubst %.cc,%,$(wildcard qt/tests/*.cc))
 endif
 
-qt/tests/qtstringtest: LDLIBS+=libwvqt.a
-qt/tests/qtstringtest: LDFLAGS+=-lqt
+qt/tests/qtstringtest: libwvqt.a
+qt/tests/%: LDLIBS+=libwvqt.a
+qt/tests/%: LDFLAGS+=-lqt
+qt/tests/%: CPPFLAGS+=-I/usr/include/qt
 
