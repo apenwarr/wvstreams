a1e8c06d51e94cc4887e0e1b4281a7eb199a49c6
tzsebe <tzsebe@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2002-11-19 01:36:11 +0000

    r3.71: fixed cases 728 and 697
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r3_71@1425 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ configfile/tests/nofile.cc
@@ -0,0 +1,63 @@
+/*
+* Worldvisions Weaver Software:
+*   Copyright (C) 1997-2002 Net Integration Technologies, Inc.
+*
+* Plays with WvConfigSectionList (without the WvConf file interface wrapper)
+*
+*/
+
+#include "wvconf.h"
+
+int main()
+{
+    WvConfigSectionList sectlist;
+    sectlist.append(new WvConfigSection("Hello"), true);
+    sectlist.append(new WvConfigSection("Hellooo"), true);
+    sectlist.append(new WvConfigSection("\n# test\n"), true);
+    sectlist.append(new WvConfigSection("aaaa"), true);
+    sectlist.append(new WvConfigSection("bbbb"), true);
+
+    WvConfigSectionList::Iter zzz(sectlist);
+    zzz.rewind();zzz.next();
+    WvConfigSection *sect = &*zzz;
+
+    if(!sect) {
+	printf("crap\n");
+	return 0;
+    }
+
+    sect->set("suck", "blah");
+    sect->set("buck", "more blah");
+    sect->set("luck", "even more");
+    sect->set("duck", "bored now");
+
+
+    zzz.rewind(); zzz.next();
+    sect = &*zzz;
+
+    WvConfigEntry *luck = (*zzz)["qwqw"];
+
+    if(!!luck)
+    {
+        WvString value = luck->value;
+	value = WvString("DIRTIED! [%s]", value);
+	luck->value = value;
+        printf("Lucky me [%s]\n", luck->value.edit());
+    }
+    else
+        zzz->quick_set("weeeee", "waaaaah");
+
+    zzz->quick_set("weeeee", "ARGH");
+
+    
+    printf("--- list everything ---\n");
+    for(zzz.rewind(); zzz.next(); )
+    {
+        printf("[%s]\n", zzz->name.edit());
+        zzz->dump(*wvcon);
+    }
+    printf("------- end list ------\n");
+
+    return 0; 
+    
+}
