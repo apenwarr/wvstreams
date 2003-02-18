015f3ae5f385e084ab079d567aef1213408fabff
sharvil <sharvil@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2003-02-18 17:53:48 +0000

    Updated test to use the new interface.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r3_73@1924 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- urlget/tests/http2test.cc
+++ urlget/tests/http2test.cc
@@ -16,7 +16,7 @@ bool want_to_die = false;
 void sighandler_die(int signum)
 {
     fprintf(stderr, "Caught signal %d; cleaning up and terminating.\n",
-	    signum);
+            signum);
     want_to_die = true;
     signal(signum, SIG_DFL);
 }
@@ -29,65 +29,65 @@ int main(int argc, char **argv)
     WvHttpPool p;
     WvString headers("");
     char *line;
-   
+
     wvcrash_setup(argv[0]);
     signal(SIGPIPE, SIG_IGN);
     signal(SIGINT, sighandler_die);
-    
+
     l.append(wvcon, false, "wvcon");
     l.append(&p, false, "pool");
-    
+
     while (!want_to_die && p.isok() && (wvcon->isok() || !p.idle()))
     {
-	if (l.select(1000))
-	{
-	    l.callback();
-	    
-	    line = wvcon->getline(0);
-	    if (line)
-	    {
-		line = trim_string(line);
-		if (!line[0])
-		    continue;
-		else if (strstr(line, ": "))
-		{
-		    // an extra http header
-		    headers = WvString("%s%s\n", headers, line);
-		    continue;
-		}
+        if (l.select(1000))
+        {
+            l.callback();
 
-		WvStream *s;
-		if (line[0] != '>')
-		    s = p.addurl(line, headers);
-		else
-		{
-		    char *ptr = strchr(line, ' ');
-		    if (!ptr)
-			continue;
-		    *ptr = 0;
-		    printf("sending file %s to url %s.\n", &line[1], ptr+1);
-		    WvFile *sendfile = new WvFile(&line[1], O_RDONLY);
-		    s = p.addputurl(ptr+1, headers, sendfile, true);
-		    l.append(sendfile, true, "sendfile");
-		}
-		
-		if (s)
-		{
-		    static int num = 0;
-		    WvFile *f = new WvFile(WvString("/tmp/url_%s", ++num), 
-					   O_CREAT|O_WRONLY|O_TRUNC);
-		    assert(!f->readable);
-		    assert(f->writable);
-		    s->autoforward(*f);
-		    l.append(s, true, "url");
-		    l.append(f, true, "outfile");
-		}
-	    }
-	}
+            line = wvcon->getline(0);
+            if (line)
+            {
+                line = trim_string(line);
+                if (!line[0])
+                    continue;
+                else if (strstr(line, ": "))
+                {
+                    // an extra http header
+                    headers = WvString("%s%s\n", headers, line);
+                    continue;
+                }
+
+                WvStream *s;
+                if (line[0] != '>')
+                    s = p.addurl(line, "GET", headers);
+                else
+                {
+                    char *ptr = strchr(line, ' ');
+                    if (!ptr)
+                        continue;
+                    *ptr = 0;
+                    printf("sending file %s to url %s.\n", &line[1], ptr+1);
+                    WvFile *sendfile = new WvFile(&line[1], O_RDONLY);
+                    s = p.addurl(ptr+1, "POST", headers, sendfile, true);
+                    l.append(sendfile, true, "sendfile");
+                }
+
+                if (s)
+                {
+                    static int num = 0;
+                    WvFile *f = new WvFile(WvString("/tmp/url_%s", ++num), 
+                            O_CREAT|O_WRONLY|O_TRUNC);
+                    assert(!f->readable);
+                    assert(f->writable);
+                    s->autoforward(*f);
+                    l.append(s, true, "url");
+                    l.append(f, true, "outfile");
+                }
+            }
+        }
     }
-    
+
     if (!p.isok() && p.geterr())
-	log("HttpPool: %s\n", p.errstr());
-    
+        log("HttpPool: %s\n", p.errstr());
+
     return 0;
 }
