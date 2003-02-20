24b2e60a6ed5c905970c7947d1088abec04497cf
apenwarr <apenwarr@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2003-02-20 01:30:04 +0000

    r3_73: merged recent changes from r3_72.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r3_73@1935 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- urlget/tests/http2test.cc
+++ urlget/tests/http2test.cc
@@ -7,7 +7,6 @@
 #include "wvhttppool.h"
 #include "wvfile.h"
 #include "strutils.h"
-#include "wvcrash.h"
 #include <signal.h>
 
 
@@ -16,7 +15,7 @@ bool want_to_die = false;
 void sighandler_die(int signum)
 {
     fprintf(stderr, "Caught signal %d; cleaning up and terminating.\n",
-            signum);
+	    signum);
     want_to_die = true;
     signal(signum, SIG_DFL);
 }
@@ -29,65 +28,49 @@ int main(int argc, char **argv)
     WvHttpPool p;
     WvString headers("");
     char *line;
-
-    wvcrash_setup(argv[0]);
+    
     signal(SIGPIPE, SIG_IGN);
     signal(SIGINT, sighandler_die);
-
-    l.append(wvcon, false, "wvcon");
-    l.append(&p, false, "pool");
-
+    
+    l.append(wvcon, false);
+    l.append(&p, false);
+    
     while (!want_to_die && p.isok() && (wvcon->isok() || !p.idle()))
     {
-        if (l.select(1000))
-        {
-            l.callback();
-
-            line = wvcon->getline(0);
-            if (line)
-            {
-                line = trim_string(line);
-                if (!line[0])
-                    continue;
-                else if (strstr(line, ": "))
-                {
-                    // an extra http header
-                    headers = WvString("%s%s\n", headers, line);
-                    continue;
-                }
-
-                WvStream *s;
-                if (line[0] != '>')
-                    s = p.addurl(line, "GET", headers);
-                else
-                {
-                    char *ptr = strchr(line, ' ');
-                    if (!ptr)
-                        continue;
-                    *ptr = 0;
-                    printf("sending file %s to url %s.\n", &line[1], ptr+1);
-                    WvFile *sendfile = new WvFile(&line[1], O_RDONLY);
-                    s = p.addurl(ptr+1, "POST", headers, sendfile, true);
-                    l.append(sendfile, true, "sendfile");
-                }
-
-                if (s)
-                {
-                    static int num = 0;
-                    WvFile *f = new WvFile(WvString("/tmp/url_%s", ++num), 
-                            O_CREAT|O_WRONLY|O_TRUNC);
-                    assert(!f->readable);
-                    assert(f->writable);
-                    s->autoforward(*f);
-                    l.append(s, true, "url");
-                    l.append(f, true, "outfile");
-                }
-            }
-        }
+	if (l.select(1000))
+	{
+	    l.callback();
+	    
+	    line = wvcon->getline(0);
+	    if (line)
+	    {
+		line = trim_string(line);
+		if (!line[0])
+		    continue;
+		else if (strstr(line, ": "))
+		{
+		    // an extra http header
+		    headers = WvString("%s%s\n", headers, line);
+		    continue;
+		}
+		
+		WvStream *s = p.addurl(line, "GET", headers);
+		if (s)
+		{
+		    static int num = 0;
+		    WvFile *f = new WvFile(WvString("/tmp/url_%s", ++num), 
+					   O_CREAT|O_WRONLY|O_TRUNC);
+		    assert(!f->readable);
+		    s->autoforward(*f);
+		    l.append(s, true);
+		    l.append(f, true);
+		}
+	    }
+	}
     }
-
+    
     if (!p.isok() && p.geterr())
-        log("HttpPool: %s\n", p.errstr());
-
+	log("HttpPool: %s\n", p.errstr());
+    
     return 0;
 }
