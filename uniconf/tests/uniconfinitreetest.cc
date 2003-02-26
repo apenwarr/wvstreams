76992caf8abeeb03252fe1cc32755008f1ea2420
dgtaylor <dgtaylor@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2003-02-26 21:31:31 +0000

    Yay to testing.  Still have to come up with a better way to do testing for the initree generator but at least it has some things to use for tests now.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@1974 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- uniconf/tests/uniconfinitreetest.cc
+++ uniconf/tests/uniconfinitreetest.cc
@@ -1,16 +1,22 @@
 // Test client for the UniIniTreeGen
+// FIXME:  Right now this just outputs everything in all the ini files.  Need to
+// come up with a better way to do this to allow for automated checking.
+
 
 #include "uniconf.h"
 #include "uniinitreegen.h"
 
 int main(int argc, char **argv)
 {
-    WvString mount_dir("/home/dgtaylor/.kde/share/config");
+    WvString mountstring("initree:initreetests");
     UniConfRoot root;
-    root.mount(WvString("initree:%s", mount_dir));
+    wvout->print("Attempting to mount:  %s\n", mountstring);
+    root.mount(mountstring);
 
-    WvString key("kwin.eventsrc/close/soundfile");
-    WvString result = root[key].get();
-    wvout->print("Result of root[\"%s\"].get() = %s\n",key, result);
+    UniConf::RecursiveIter i(root);
+    for (i.rewind(); i.next();)
+    {
+        wvout->print("Key:  %s.  Value:  %s.\n",i->fullkey(), i->get());
+    }
     return 0;
 }
--- uniconf/unifiletreegen.cc
+++ uniconf/unifiletreegen.cc
@@ -42,7 +42,7 @@ bool UniConfFileTreeGen::refresh()
         }
     }
 
-//    UniMountTreeGen::refresh();
+    UniMountTreeGen::refresh();
     return true;
 }
 
