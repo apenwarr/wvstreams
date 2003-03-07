502b4475751c8909fefcd6452a60f06027d10e25
drheld <drheld@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2003-03-07 18:26:18 +0000

    Added WvScatterHash (kind of mostly done...) as a faster(?) hash table that can automatically increase its size as needed and add elements with no memory allocation.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@2051 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ include/wvscatterhash.h
@@ -0,0 +1,226 @@
+#ifndef __WVSCATTERHASH_H
+#define __WVSCATTERHASH_H
+
+#include <sys/types.h>
+#include "wvhashtable.h"
+
+
+#define MAX_LOAD_FACTOR 0.5
+#define IS_OCCUPIED(x) (x.status >> 1)
+#define IS_AUTO_FREE(x) (x.status == 3)
+#define IS_DELETED(x) (x.status == 1)
+
+
+class WvScatterHashBase
+{
+public:
+
+    WvScatterHashBase(unsigned _numslots);
+    virtual ~WvScatterHashBase() { }
+
+    struct pair
+    {
+        void *data;
+        unsigned status : 2;
+    };
+
+    static struct pair null_pair;
+    static const unsigned prime_numbers[];
+
+    size_t count() const { return num; }
+    bool isempty() const { return !num; }
+
+  
+
+    /******* IterBase ******/
+    class IterBase
+    {
+    public:
+        IterBase(WvScatterHashBase &_table) : table(&_table) { }
+
+        IterBase(const IterBase &other)
+            : table(other.table), index(other.index) { }
+
+        void rewind() { index = 0; }
+        bool next()
+        {
+            while (++index <= table->numslots &&
+                   !IS_OCCUPIED(table->slots[index-1])) { }
+
+            if (index <= table->numslots) return true;
+            return false;
+        }
+
+        bool get_autofree()
+            { return IS_AUTO_FREE(table->slots[index-1]); }
+
+        void set_autofree(bool auto_free)
+            { table->slots[index-1].status = auto_free ? 3 : 2; }
+
+    protected:
+        void *get() const { return table->slots[index-1].data; }
+
+        WvScatterHashBase *table;
+        unsigned index;
+    };
+
+
+protected:
+    virtual unsigned do_hash(const void *data) = 0;
+    virtual void do_delete(void *data) = 0;
+
+    friend class IterBase;
+
+    pair *slots;
+    int prime_index;
+    unsigned numslots;
+
+    pair *genfind(const void *data, unsigned hash);
+    void _add(void *data, bool auto_free);
+    void _add(void *data, unsigned hash, bool auto_free);
+    void _remove(const void *data, unsigned hash);
+    void _zap(bool destructor = false);
+    void _set_autofree(const void *data, unsigned hash, bool auto_free);
+    bool _get_autofree(const void *data, unsigned hash);
+
+    virtual bool compare(const void *key, const void *elem) const = 0;
+
+
+private:
+    void rebuild();
+    long curhash(unsigned hash, unsigned attempt)
+        //{ return (hash + attempt * attempt) % numslots; }
+        { return (hash + attempt*((hash % (numslots - 1) + 1))) % numslots; }
+
+    size_t used;
+    size_t num;
+};
+
+
+template <
+    class T,                                            // element type
+    class K,                                            // key type
+    class Accessor,                                     // element to key
+    template <class> class Comparator = OpEqComp        // comparison func
+>
+class WvScatterHash : public WvScatterHashBase
+{
+protected:
+    typedef Comparator<K> MyComparator;
+
+    virtual bool compare(const void *key, const void *elem) const
+        { return MyComparator::compare((const K *)key,
+                Accessor::get_key((const T *)elem)); }
+
+    unsigned hash(const T *data)
+        { return WvHash(*Accessor::get_key(data)); }
+
+    virtual unsigned do_hash(const void *data)
+        { return hash((const T *)data); }
+
+    virtual void do_delete(void *data)
+        { delete (T *)data; }    
+
+public:
+    WvScatterHash(unsigned _numslots) : WvScatterHashBase(_numslots) { }
+    virtual ~WvScatterHash() { _zap(true); }
+
+    T *operator[] (const K &key)
+        { return (T *)(genfind(&key, WvHash(key))->data); }
+
+    void add(const T *data, bool auto_free = false)
+        { _add((void *)data, hash(data), auto_free); }
+
+    void remove(const T *data)
+        { _remove(Accessor::get_key(data), hash(data)); }
+
+    void set_autofree(const T *data, bool auto_free)
+        { _set_autofree(Accessor::get_key(data), hash(data), auto_free); }
+
+    bool get_autofree(const T *data)
+        { _get_autofree(Accessor::get_key(data), hash(data)); }
+
+    void zap()
+        { _zap(); }
+
+
+    class Iter : public WvScatterHashBase::IterBase
+    {
+    public:
+        Iter(WvScatterHash &_table) : IterBase(_table) { }
+        Iter(const Iter &other) : IterBase(other) { }
+
+        int *getstatus() { return &slots[index-1].status; }
+
+        T *ptr() const
+            { return (T *)(get()); }
+
+        WvIterStuff(T);
+    };
+};
+
+
+#define DeclareWvScatterDict2(_classname_,  _type_, _ftype_, _field_)     \
+        __WvScatterDict_base(_classname_, _type_, _ftype_, &obj->_field_)
+
+#define DeclareWvScatterDict(_type_, _ftype_, _field_)                    \
+        DeclareWvScatterDict2(_type_##Dict, _type_, _ftype_, _field_)
+
+#define DeclareWvScatterTable2(_classname_, _type_)                       \
+        __WvScatterDict_base(_classname_, _type_, _type_, obj)
+
+#define DeclareWvScatterTable(_type_)                                     \
+        DeclareWvScatterTable2(_type_##Table, _type_)
+
+
+#define __WvScatterDict_base(_classname_, _type_, _ftype_, _field_)       \
+    template <class T, class K>                                           \
+    struct _classname_##Accessor                                          \
+    {                                                                     \
+        static const K *get_key(const T *obj)                             \
+            { return _field_; }                                           \
+    };                                                                    \
+                                                                          \
+    typedef WvScatterHash<_type_, _ftype_,                                \
+             _classname_##Accessor<_type_, _ftype_> > _classname_
+
+#endif //_WVSCATTERHASH_H
+
+
+// *****************************
+// Main map template
+template
+<
+    typename TKey,
+    typename TData,
+    template <class> class Comparator = OpEqComp
+>
+class WvScatterMap : public WvScatterHash<WvPair<TKey, TData>, TKey,
+        WvScatterMap<TKey, TData, Comparator>, Comparator>
+{
+protected:
+    typedef WvPair<TKey, TData> MyPair;
+    typedef WvScatterMap<TKey, TData, Comparator> MyMap;
+    typedef WvScatterHash<MyPair, TKey, MyMap, Comparator> MyHashTable;
+
+    // accessor mothod for WvHashTable to use
+    friend class MyHashTable;
+    static const TKey *get_key(const MyPair *obj)
+        { return &obj->key; }
+
+public:
+    WvScatterMap(int s) : MyHashTable(s)    { };
+    /* May return NULL!! */
+    TData *find(const TKey &key)
+    {
+        MyPair* p = MyHashTable::operator[](key);
+        return p ? &p->data : (TData*)NULL;
+    }
+    TData *operator[](const TKey &key)
+        { return find(key); }
+    void add(const TKey &key, const TData &data, bool auto_free = false)
+        { MyHashTable::add(new MyPair(key, data, auto_free), true); }
+    void remove(const TKey &key)
+        { MyHashTable::remove(MyHashTable::operator[](key)); }
+    typedef typename MyHashTable::Iter Iter;
+};
---
+++ utils/wvscatterhash.cc
@@ -0,0 +1,128 @@
+#include "wvscatterhash.h"
+#include <string.h>
+
+const unsigned WvScatterHashBase::prime_numbers[] = {2u, 5u, 11u, 17u, 31u, 67u, 127u, 251u, 509u, 1021u, 2039u, 4093u, 8191u, 16381u, 32749u, 65521u, 131071u, 262139u, 524287u, 1048573u, 2097143u, 4194301u, 8388593u, 16777213u, 33554393u, 67108859u, 134217689u, 268435399u, 536870909u, 1073741789u, 2147483647u, 4294967281u};
+
+WvScatterHashBase::pair WvScatterHashBase::null_pair;
+
+WvScatterHashBase::WvScatterHashBase(unsigned _numslots)
+{
+    num = 0;
+    used = 0;
+
+    prime_index = 1;
+    while ((_numslots >>= 1) != 0)
+        prime_index++;
+
+    numslots = prime_numbers[prime_index];
+    slots = new pair[numslots];
+    memset(slots, 0, numslots * sizeof(pair));
+}
+
+void WvScatterHashBase::rebuild()
+{
+    unsigned oldnumslots = numslots;
+
+    if (numslots * MAX_LOAD_FACTOR <= num) 
+        numslots = prime_numbers[++prime_index];
+
+    pair *tmpslots = slots;
+    slots = new pair[numslots];
+    used = num = 0;
+
+    for (unsigned i = 0; i < oldnumslots; i++)
+    {
+        if (IS_OCCUPIED(tmpslots[i]))
+            _add(tmpslots[i].data, IS_AUTO_FREE(tmpslots[i]));
+    }
+
+    delete[] tmpslots;
+}
+
+void WvScatterHashBase::_add(void *data, bool auto_free)
+{
+    _add(data, do_hash(data), auto_free);
+}
+
+void WvScatterHashBase::_add(void *data, unsigned hash, bool auto_free)
+{
+    if (numslots * MAX_LOAD_FACTOR <= used)
+        rebuild();
+
+    unsigned slot = hash % numslots;
+    unsigned attempt = 0;
+
+    while (IS_OCCUPIED(slots[slot]))
+        slot = curhash(hash, ++attempt);
+
+    num++;
+    if (!IS_DELETED(slots[slot]))
+        used++;
+
+    slots[slot].data = data;
+    slots[slot].status = auto_free ? 3 : 2;
+}
+
+void WvScatterHashBase::_remove(const void *data, unsigned hash)
+{
+    pair *res = genfind(data, hash);
+
+    if (res)
+    {
+        if (IS_AUTO_FREE((*res)))
+            do_delete(res->data);
+        res->status = 1;
+        num--;
+    }
+}
+
+void WvScatterHashBase::_zap(bool destructor)
+{
+    for (unsigned i = 0; i < numslots; i++)
+    {
+        if (IS_AUTO_FREE(slots[i]))
+            do_delete(slots[i].data);
+    }
+
+    delete[] slots;
+
+    if (!destructor)
+        slots = new pair[numslots];
+}
+
+void WvScatterHashBase::_set_autofree(const void *data,
+    unsigned hash, bool auto_free)
+{
+    pair *res = genfind(data, hash);
+
+    if (res)
+        res->status = auto_free ? 3 : 2;
+}
+
+bool WvScatterHashBase::_get_autofree(const void *data, unsigned hash)
+{
+    pair *res = genfind(data, hash);
+
+    if (res)
+        return IS_AUTO_FREE((*res));
+
+    assert(0 && "You checked auto_free of a nonexistant thing.");
+    return false;
+}
+
+struct WvScatterHashBase::pair *WvScatterHashBase::genfind
+    (const void *data, unsigned hash)
+{
+    unsigned slot = hash % numslots;
+    int attempt = 0;
+
+    while (slots[slot].status)
+    {
+        if (IS_OCCUPIED(slots[slot]) && compare(data, slots[slot].data))
+            return &slots[slot];
+
+        slot = curhash(hash, ++attempt);
+    } 
+
+    return &null_pair;
+}
