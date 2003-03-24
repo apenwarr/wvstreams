0a6fdd0e9af770c15f98b251c30255fac8db0339
jnc <jnc@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2003-03-24 20:28:21 +0000

    HEAD: Add files missed in last two checkins.  (Unfortunately, this means there's no record of the user conversation stuff, since it was never checked in in the first place.)
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@2129 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ uniconf/daemon/uniconfpamconn.cc
@@ -0,0 +1,141 @@
+/*
+ * Worldvisions Weaver Software:
+ *   Copyright (C) 1997-2002 Net Integration Technologies, Inc.
+ *
+ * Manages a UniConf daemon session, first authenticating with PAM.
+ */
+
+#include "uniconfpamconn.h"
+
+#ifdef HAVE_SECURITY_PAM_APPL_H
+
+#include "wvtclstring.h"
+#include "wvlog.h"
+
+
+const int PAM_START_STEP = 1;
+const int PAM_AUTH_STEP = 2;
+const int PAM_ACCT_STEP = 3;
+const int PAM_SETCRED_STEP = 4;
+const int PAM_SESSION_STEP = 5;
+
+
+const char *UniConfPamConn::PAM_SERVICE_NAME = "uniconfdaemon";
+
+
+UniConfPamConn::UniConfPamConn(WvStream *s, const UniConf &root) :
+    UniConfDaemonConn(s, root),
+    pamh(NULL), pam_status(PAM_SUCCESS), session_exists(false)
+{
+}
+
+
+UniConfPamConn::~UniConfPamConn()
+{
+    if (session_exists)
+        pam_close_session(pamh, 0);
+    pam_end(pamh, pam_status);
+}
+
+
+void UniConfPamConn::startup()
+{
+    UniConfDaemonConn::startup();
+
+    WvLog("UniConfPamConn", WvLog::Debug);
+
+    struct pam_conv c;
+    c.conv = UniConfPamConn::noconv;
+    c.appdata_ptr = NULL;
+    pam_status = pam_start(PAM_SERVICE_NAME, NULL, &c, &pamh);
+    if (!check_pam_result("startup")) return;
+
+    pam_status = pam_authenticate(pamh, PAM_DISALLOW_NULL_AUTHTOK);
+    if (!check_pam_result("authentication")) return;
+
+    pam_status = pam_acct_mgmt(pamh, 0);
+    if (!check_pam_result("authorization")) return;
+        
+    pam_status = pam_setcred(pamh, PAM_ESTABLISH_CRED);
+    if (!check_pam_result("credentials")) return;
+        
+    pam_status = pam_open_session(pamh, 0);
+    if (!check_pam_result("session open")) return;
+
+    session_exists = true;
+}
+
+
+bool UniConfPamConn::check_pam_result(WvStringParm s)
+{
+    if (pam_status == PAM_SUCCESS)
+    {
+        log("PAM %s succeeded\n", s);
+        return true;
+    }
+    else
+    {
+        log("PAM %s FAILED: %s\n", s, pam_status);
+        writefail(wvtcl_escape("Authorization failed"));
+        return false;
+    }
+}
+
+
+bool UniConfPamConn::isok() const
+{
+    return ((pam_status == PAM_SUCCESS || pam_status == PAM_INCOMPLETE)
+            && UniConfDaemonConn::isok());
+}
+
+
+int UniConfPamConn::noconv(int num_msg, const struct pam_message **msgm,
+        struct pam_response **response, void *userdata)
+{
+    // if you need to ask things, it won't work
+    return PAM_CONV_ERR;
+}
+
+
+void UniConfPamConn::do_get(const UniConfKey &key)
+{
+    if (session_exists)
+        UniConfDaemonConn::do_get(key);
+    else
+        writefail("Not authenticated");
+}
+
+
+void UniConfPamConn::do_set(const UniConfKey &key, WvStringParm value)
+{
+    if (session_exists)
+        UniConfDaemonConn::do_set(key, value);
+    // don't write fail because this doesn't expect a response
+}
+
+void UniConfPamConn::do_remove(const UniConfKey &key)
+{
+    if (session_exists)
+        UniConfDaemonConn::do_remove(key);
+    // don't write fail because this doesn't expect a response
+}
+
+
+void UniConfPamConn::do_subtree(const UniConfKey &key)
+{
+    if (session_exists)
+        UniConfDaemonConn::do_subtree(key);
+    else
+        writefail("Not authenticated");
+}
+
+
+void UniConfPamConn::do_haschildren(const UniConfKey &key)
+{
+    if (session_exists)
+        UniConfDaemonConn::do_haschildren(key);
+    else
+        writefail("Not authenticated");
+}
+
+#endif // HAVE_SECURITY_PAM_APPL_H
