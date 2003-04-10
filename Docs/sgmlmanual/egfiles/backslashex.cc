9fcb50f4719fbb227dab03071f4021fadd2432e1
kindness <kindness@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2003-04-10 20:09:12 +0000

    HEAD: more wvstreams manuals for utils, wvstring, wvlinklist and examples
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@2240 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ Docs/sgmlmanual/egfiles/backslashex.cc
@@ -0,0 +1,27 @@
+#include "wvbackslash.h"
+#include "wvbufbase.h"
+
+int main()
+{
+   WvBackslashEncoder *enc;
+   enc = new WvBackslashEncoder("abcd");
+   //enc contains the letters you want to escape (add a backslash to)
+
+   //If you want a decoder, then enc has to be initialiazed like this:
+   //enc = new WvBackslashDecoder();
+
+   WvInPlaceBuf to_encode(20);
+   WvInPlaceBuf out(40);
+
+   to_encode.put("Test abcdefg",12);
+   //to_encode contains the string to be encoded
+   //(added a backslash(es) at the correct spot)
+
+   if (enc->encode(to_encode, out, true,true))
+     printf ("This is the result: %s\n", (char *) out.get(1));
+
+   //it will display on screen:
+   //This is the result: Test \a\b\c\defg
+
+   return 0;
+}
