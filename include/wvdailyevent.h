b9960072fe98a258a49b736ac1fbe68ac5a6006d
apenwarr <apenwarr@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2003-07-09 03:10:53 +0000

    r3_72a: wvdailyevent waits a minimum of one full period before setting off an event.  That way it never gets in the way of weaver startup.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r3_72a@2536 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- include/wvdailyevent.h
+++ include/wvdailyevent.h
@@ -43,6 +43,8 @@ private:
     bool    need_reset;
     int     last_hour;
     int     last_minute;
+    
+    time_t  not_until;
 };
 
 #endif
--- streams/wvdailyevent.cc
+++ streams/wvdailyevent.cc
@@ -12,7 +12,6 @@
  * extended one day when someone cares.
  *
  */
-
 #include "wvstream.h"
 #include "wvdailyevent.h"
 
@@ -20,30 +19,34 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-WvDailyEvent::WvDailyEvent( int _first_hour, int _num_per_day )
-/*************************************************************/
-: first_hour( _first_hour ), num_per_day( _num_per_day )
+WvDailyEvent::WvDailyEvent(int _first_hour, int _num_per_day)
 {
     need_reset = false;
     last_hour = -1;
     last_minute = -1;
+    configure(_first_hour, _num_per_day);
 }
 
-bool WvDailyEvent::pre_select( SelectInfo& si )
-/*********************************************/
+
 // we're "ready" if the time just changed to "first_hour" o'clock,
 // OR if the time just changed to "first_hour" o'clock plus a multiple of
 // 24*60 / num_per_day minutes.
+bool WvDailyEvent::pre_select(SelectInfo &si)
 {
-    time_t      now;
-    struct tm * tnow;
-
-    now  = time( NULL );
-    tnow = localtime( &now );
+    time_t now;
+    struct tm *tnow;
+    
+    now  = time(NULL);
+    tnow = localtime(&now);
+    
+    // too soon after configuration - skip the event
+    if (now < not_until)
+	return false;
 
     // for a specific hour
-    if( tnow->tm_hour == first_hour ) {
-        if( (first_hour-1) % 24 == last_hour )
+    if (tnow->tm_hour == first_hour)
+    {
+        if ((first_hour-1) % 24 == last_hour)
             need_reset = true;
     }
     last_hour = tnow->tm_hour;
@@ -51,47 +54,57 @@ bool WvDailyEvent::pre_select( SelectInfo& si )
     // for a number of times a day
     // use the daily "first_hour" as an offset.  (if first_hour is 3, and
     // num_per_day is 2, we want to tick at 3 am and 3 pm.)
-    int this_minute = ( ( tnow->tm_hour - first_hour )%24 )*60 + tnow->tm_min;
-    if( num_per_day ) {
+    int this_minute = ((tnow->tm_hour - first_hour) % 24) * 60 + tnow->tm_min;
+    if (num_per_day)
+    {
         int min_between = 24*60 / num_per_day;
-        if( this_minute % min_between == 0 ) {
-            if( last_minute != this_minute )
+        if ((this_minute % min_between) == 0)
+	{
+            if (last_minute != this_minute)
                 need_reset = true;
         }
     }
     last_minute = this_minute;
 
-    return( need_reset );
+    return need_reset;
 }
 
+
 bool WvDailyEvent::post_select( SelectInfo& si )
-/**********************************************/
 {
     return( need_reset );
 }
 
+
 void WvDailyEvent::execute()
-/**************************/
 {
     WvStream::execute();
     reset();
 }
 
+
 void WvDailyEvent::reset()
-/************************/
 {
     need_reset = false;
 }
 
+
 bool WvDailyEvent::isok() const
-/*****************************/
 {
-    return( true );
+    return true;
 }
 
-void WvDailyEvent::configure( int _first_hour, int _num_per_day )
-/***************************************************************/
+
+void WvDailyEvent::configure(int _first_hour, int _num_per_day)
 {
     first_hour = _first_hour;
     num_per_day = _num_per_day;
+    if (num_per_day < 0)
+	num_per_day = 1;
+    
+    // don't start until at least one period has gone by
+    time_t max = (24*60*60)/(num_per_day + 1);
+    if (max > 6*60*60)
+	max = 6*60*60; // unless that's a very long time
+    not_until = time(NULL) + max;
 }
