2b5bd3d48fb26e3eecd1e87055f0f180ac5e676c
apenwarr <apenwarr@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2003-08-05 03:36:40 +0000

    Cleaning conttest.  Recursion now officially causes an assert() failure, since I'm pretty sure there's nothing that makes more sense than that. According to jnc's prof, that makes WvCont an implementation of semi-coroutines.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@2630 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- utils/tests/conttest.cc
+++ utils/tests/conttest.cc
@@ -2,13 +2,20 @@
 #include "wvcallback.h"
 #include <stdio.h>
 
-typedef int Ret;
-typedef int P1;
-typedef WvCallback<Ret, P1> RealCallback;
-
 
 class WvCont
 {
+public:
+    /**
+     * These are hardcoded to int, because I'm too lazy to templatize this.
+     * Most people won't use the return and parameter values anyhow.
+     */
+    typedef int R;
+    typedef int P1;
+
+    typedef WvCallback<R, P1> Callback;
+    
+private:
     /**
      * When we copy a WvCont, we increase the reference count of the 'data'
      * member rather than copying it.  That makes it so every copy of a given
@@ -34,7 +41,7 @@ public:
      * can be used in place of that callback, and stored in a callback of
      * the same data type.
      */
-    WvCont(const RealCallback &cb);
+    WvCont(const Callback &cb);
     
     /** Copy constructor. */
     WvCont(const WvCont &cb);
@@ -47,11 +54,14 @@ public:
      * parameter to the function, and returning Ret, the argument of yield()
      * or the return value of the function.
      */
-    Ret operator() (P1 p1);
+    R operator() (P1 p1);
     
     // the following are static because a function doesn't really know
     // which WvCont it belongs to, and only one WvCont can be the "current"
     // one globally in an application anyway.
+    // 
+    // Unfortunately this prevents us from assert()ing that you're in the
+    // context you think you are.
     
     /**
      * "return" from the current callback, giving value 'ret' to the person
@@ -59,33 +69,42 @@ public:
      * had returned, and the parameter to the callback is the value of
      * yield().
      */
-    static P1 yield(Ret ret);
+    static P1 yield(R ret);
+    
+    /**
+     * Tell us if the current context is "okay", that is, not trying to
+     * die. If !isok(), you shouldn't yield(), because the caller is just
+     * going to keep calling you until you die.  Return as soon as you can.
+     */
     static bool isok();
 };
 
 
 struct WvCont::Data
 {
-    int links;
-    RealCallback cb;
+    int links;          // the refcount of this Data object
+    int mydepth;        // this task's depth in the call stack
+    bool finishing;     // true if we're trying to terminate this task ASAP
     WvTaskMan *taskman;
     WvTask *task;
-    bool finishing;
-    Ret ret;
+    
+    Callback cb;        // the callback we want to call inside our WvTask
+    R ret;
     P1 p1;
     
-    Data(const RealCallback &_cb) : cb(_cb)
-        { links = 1; taskman = WvTaskMan::get();
+    Data(const Callback &_cb) : cb(_cb)
+        { links = 1; taskman = WvTaskMan::get(); 
 	     task = NULL; finishing = false; report(); }
     ~Data()
         { assert(!links); taskman->unlink(); report(); }
+
     void link()
         { links++; report(); }
     void unlink()
         { links--; report(); if (!links) delete this; }
     
     void report()
-        { printf("%p: links=%d\n", this, links); }
+        { /* printf("%p: links=%d\n", this, links); */ }
 };
 
 
@@ -100,7 +119,7 @@ WvCont::WvCont(const WvCont &cb)
 }
 
 
-WvCont::WvCont(const RealCallback &cb)
+WvCont::WvCont(const Callback &cb)
 {
     data = new Data(cb);
 }
@@ -122,32 +141,39 @@ WvCont::~WvCont()
 }
 
 
+// note: assumes data->task is already running!
 void WvCont::call()
 {
     Data *olddata = curdata;
     curdata = data;
     
     // enforce the call stack.  If we didn't do this, a yield() five calls
-    // deep would return to the very top.
+    // deep would return to the very top, rather to the second-innermost
+    // context.
     // 
-    // There are more simple-minded ways to do this than using a stack, but
-    // they can get you in trouble if you, for example, have a, which calls
-    // b, which calls a, which calls c, which yields.
+    // Note that this implementation has the interesting side-effect of
+    // short-circuiting recursion (a calls b, b calls c, c calls a), since
+    // calling 'a' if it's already running means the same as "yield all the
+    // way back to a", and this loop enforces one-level-at-a-time yielding.
     // 
-    // FIXME: something about this is insane.
-    int mydepth = ++taskdepth;
+    // Because that behaviour is probably undesirable, we make 'mydepth' into
+    // a member variable instead of just putting it on the stack.  This is
+    // only needed so that we can have the assert().
+    assert(!data->mydepth);
+    data->mydepth = ++taskdepth;
     do
     {
 	data->taskman->run(*data->task);
-    } while (taskdepth > mydepth);
-    assert(taskdepth == mydepth);
+    } while (taskdepth > data->mydepth);
+    assert(taskdepth == data->mydepth);
     taskdepth--;
+    data->mydepth = 0;
 
     curdata = olddata;
 }
 
 
-int WvCont::operator() (P1 p1)
+WvCont::R WvCont::operator() (P1 p1)
 {
     data->ret = -42;
     
@@ -162,10 +188,11 @@ int WvCont::operator() (P1 p1)
 }
 
 
-P1 WvCont::yield(Ret ret)
+WvCont::P1 WvCont::yield(R ret)
 {
     assert(curdata);
     assert(curdata->task == curdata->taskman->whoami());
+    assert(isok()); // this assertion is a bit aggressive...
     curdata->ret = ret;
     curdata->taskman->yield();
     return curdata->p1;
@@ -184,6 +211,7 @@ void WvCont::bouncer(void *userdata)
 {
     Data *data = (Data *)userdata;
     
+    // DON'T BE FOOLED!
     // all yield() calls stay inside the inner function; our return value
     // is only for the final run after data->cb() returns.
     data->ret = data->cb(data->p1);
@@ -192,7 +220,7 @@ void WvCont::bouncer(void *userdata)
     
 static int nonfunc(int x)
 {
-    return -1;
+    return 1234560000 + x;
 }
 
 
@@ -208,14 +236,15 @@ class Honk
 {
 public:
     const char *id;
-    RealCallback cb;
+    
+    WvCont::Callback cb;
     
     Honk(const char *_id)
         { id = _id; }
     
     void honk_at(Honk &a)
     {
-	cb = WvCont(WvBoundCallback<RealCallback, Honk &>
+	cb = WvCont(WvBoundCallback<WvCont::Callback, Honk &>
 		    (this, &Honk::honker, a));
     }
 
@@ -239,23 +268,26 @@ private:
 
 int main()
 {
+    typedef WvCallback<int, int> CbType;
+    
     // basic functionality (including nested tasks)
     {
-	RealCallback cbx = func; // not runnable itself: no yield allowed
-	RealCallback cb1 = WvCont(cbx); // a subtask
-	RealCallback cb2 = WvCont(cb1); // another subtask wrapping the first
-	RealCallback cb3 = cb2; // a copy of the second subtask
+	CbType cbx = func; // not runnable itself: no yield allowed
+	CbType cb1 = WvCont(cbx); // a subtask
+	CbType cb2 = WvCont(cb1); // another subtask on top
+	CbType cb3 = cb2; // a copy of the second subtask
 	// note that in the above, there's really only one context in which
 	// 'func' actually gets called; there are no parallel running 'func's.
+	// cb2's task calls into cb1's task, however.
     
 	printf("zot1: %d\n", cb1(100));
 	printf("zot1: %d\n", cb2(200));
 	printf("zot1: %d\n", cb3(300));
-	cb2 = nonfunc;
+	cb1 = WvCont(nonfunc);
 	printf("zot2: %d\n", cb1(400));
 	printf("zot2: %d\n", cb2(500));
 	printf("zot2: %d\n", cb3(600));
-	cb1 = WvCont(nonfunc);
+	cb2 = nonfunc;
 	printf("zot3: %d\n", cb1(700));
 	printf("zot3: %d\n", cb2(800));
 	printf("zot3: %d\n", cb3(900));
@@ -271,20 +303,22 @@ int main()
     // 
     // The *desired* behaviour here is the same as with real recursive
     // function calls: if a calls b who calls c, and then c calls a again,
-    // then a should do its thing, return (or yield), when c will finish,
+    // then a should do its thing, return (or yield), then c will finish,
     // yield, then b will finish, yield, and then a will have a chance to run
     // again.
     // 
-    // In old wvstreams, we would short-circuit the recursion (the inner a
-    // would yield immediately without doing anything).  This is easy to
-    // implement, but causes problems if c actually expects a to do something.
+    // In old wvstreams, we would silently short-circuit the recursion (the
+    // inner a would yield immediately without doing anything).  This is
+    // easy to implement, but causes problems if c actually expects a to do
+    // something.
     // 
     // Unfortunately, the semantics of this are tricky with continuations:
     // when we call the inner a, we re-enter its context, but that context
     // is waiting for b to return.  It can't do anything unless b returns,
-    // so what can we do?  I guess we can pretend that b *did* return,
-    // let a run, then rewind and *actually* run b, and then... run a again?
-    // Or leave it out?
+    // so what can we do?
+    // 
+    // ...we assert() instead.  So expect an assertion failure below.
+    printf("Expect an assertion failure shortly!\n");
     {
 	Honk h1("honk1"), h2("honk2"), h3("honk3");
 	h1.honk_at(h2);
