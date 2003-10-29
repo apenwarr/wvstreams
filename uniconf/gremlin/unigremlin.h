de010709bedd0a057b66e7c2067355336024dbce
mrwise <mrwise@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2003-10-29 23:53:28 +0000

    head: first rendition of the UniConfGremlin, see UniConfGremlin in the wiki for details
    BUGZID:2684
    
    
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@3013 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ uniconf/gremlin/unigremlin.h
@@ -0,0 +1,56 @@
+#ifndef __UNIGREMLIN_H
+#define __UNIGREMLIN_H
+
+#include "wvstring.h"
+#include "wvhashtable.h"
+#include "uniconfroot.h"
+#include "uniconfkey.h"
+
+struct Victim
+{
+    int i;
+    int type;
+    WvString name;
+    Victim(int _i, int _type, WvStringParm _name)
+    { i = _i; type = _type; name = _name; }
+};
+DeclareWvDict(Victim, int, i);
+
+class UniConfGremlin
+{
+public:
+    // runlevels are defined in curr_runlevel()
+    UniConfGremlin(WvString moniker, const UniConfKey key = "", 
+            int max_runlevel = 5);
+    
+    // FIXME: use_valid_data does not work when set to true yet
+    void start(unsigned int seed = 0);
+    void test(); 
+    WvString status();
+private:
+    // creates a list of the keys and expected values in the UniConf subtree
+    void find_victims(UniConfKey _key);
+
+    void change_value(bool use_valid_data = false);
+    void add_value();
+    
+    // begin randomly changing key values, using only valid data if
+    // use_valid_data is true
+    void start_trouble(int curr_runlevel);
+    
+    // returns what it thinks is the expected data for this element
+    int inspect(WvString element);
+    bool is_bool(char *elem);
+    
+    // returns "" if type was not a known string type
+    WvString rand_str(int type);
+    WvString type_name(int type); 
+    WvString curr_runlevel();
+    
+    UniConfRoot cfg;
+    UniConfKey key;
+    static VictimDict victims;
+    int num_victims, max_runlevel, runlevel;
+    WvString last_change;
+};
+#endif
