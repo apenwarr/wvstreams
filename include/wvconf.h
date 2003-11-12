95beb47f4a43e725e1bd82ce3646e211ee4f23df
sfllaw <sfllaw@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2003-11-12 17:22:10 +0000

    r3_75: Hold the pointer (*wvauthd) to the global WvAuthDaemon within WvConf.  This is an ugly hack, but it allows WvConf easy access to the daemon without having to change the interface of WvConf.
    I had considered making WvConf contain virtual check_passwd()
    functions; but this causes the linker to complain bitterly that not
    all symbols are found.  We don't ship wvconf.cc with implementations
    of get_passwd(), set_passwd(), check_passwd() or user_exists(); so
    this is not a good solution.
    
    WvConf itself does not manage *wvauthd but WvAuthDaemonSvc does.  This
    service is a friend of WvConf, which allows this to happen in a
    protected way.
    
    BUGZID: 1794
    
    
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r3_75@3099 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- include/wvconf.h
+++ include/wvconf.h
@@ -81,6 +81,10 @@ public:
 DeclareWvList(WvConfCallbackInfo);
 DeclareWvList(WvConfigSection);
 
+
+class WvAuthDaemon;
+class WvAuthDaemonSvc;
+
 /**
  * WvConf configuration file management class: used to read/write config
  * files that are formatted in the style of Windows .ini files.
@@ -229,6 +233,19 @@ private:
 
     char *parse_section(char *s);
     char *parse_value(char *s);
+
+/* The following is an ugly hack, but since WvConf is being
+ * deprecated, we don't care.
+ * 
+ * It seems that check_passwd() and user_exists() need to talk to a
+ * WvAuthDaemon.  However, making them virtual functions would break since
+ * everyone else has to implement them.  So we'll its pointer and accessors
+ * here.
+ */
+private:
+    WvAuthDaemon *wvauthd;	// Authentication Daemon
+public:
+    friend WvAuthDaemonSvc;
 };
 
 
