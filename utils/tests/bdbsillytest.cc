12f685603b507318268d16385a3f5ade684455d6
(no author) <(no author)@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2003-11-29 00:40:46 +0000

    This commit was manufactured by cvs2svn to create branch 'wvstreams_unittest'.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/wvstreams_unittest@3252 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ utils/tests/bdbsillytest.cc
@@ -0,0 +1,39 @@
+/*
+ * Do some repetitive stuff that *shouldn't* stress bdbhash very much, so
+ * that somebody can strace it and see if it does.
+ */
+#include "wvbdbhash.h"
+
+typedef WvBdbHash<int, WvString> MyHash;
+
+int main()
+{
+    MyHash h("/tmp/blahdb");
+    int num;
+    
+    h.zap();
+    
+    for (int count = 0; count < 100000; count++)
+    {
+	h.add(count, WvString("String number %s", count), false);
+	fprintf(stderr, ".");
+    }
+    
+    num = 0;
+    MyHash::Iter i(h);
+    for (i.rewind(), num = 0; i.next(); num++)
+    {
+	for (int count = 0; count < 10; count++)
+	{
+	    int cur = i.key();
+	    assert(num == cur);
+	    fprintf(stderr, ":");
+	    WvString scur(*i);
+	    WvString snum("String number %s", num);
+	    assert(snum == scur);
+	    fprintf(stderr, ".");
+	}
+	
+	fprintf(stderr, "!");
+    }
+}
