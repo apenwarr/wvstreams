daf6b98174f857633cac2d0af20f9ae74656d41e
mrwise <mrwise@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2003-12-03 20:19:51 +0000

    wvstreams_unittest: old style tests aren't fully merged over yet, but kenrose is working on writing his own tests, so committing an ifdef'd out version of what I have so far, so we don't get too out of sync
    BUGZID:2933
    
    
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/wvstreams_unittest@3316 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ streams/t/wvfdstream.t.cc
@@ -0,0 +1,78 @@
+#include "wvtest.h"
+#include "wvstreamclone.h"
+#include "wvfdstream.h"
+#include "wvlog.h"
+
+//FIXME: absolutely simple simple test right now, built from closeflushtest
+class SillyStream : public WvFDStream
+{
+public:
+    int count;
+    
+    SillyStream() : WvFDStream(1)
+    {
+	count = 0;
+    }
+    
+    virtual size_t uwrite(const void *buf, size_t size)
+    {
+	++count;
+	fprintf(stderr, "uwrite #%d (%d bytes)\n", count, size);
+	if (count == 2)
+	    close(); // pretend we had a socket error
+	return 0;
+    }
+    
+    virtual bool post_select(SelectInfo &si)
+    {
+	fprintf(stderr, "post_select(%d,%d)\n",
+		si.wants.readable, si.wants.writable);
+	return WvFDStream::post_select(si) || true;
+    }
+    
+    virtual void close()
+    {
+	fprintf(stderr, "closing.\n");
+//	WvFDStream::close();
+    }
+    
+    size_t obu()
+    {
+	return outbuf.used();
+    }
+};
+
+#ifdef TEST_IS_FULLY_CONVERTED
+WVTEST_MAIN("old-style")
+{
+    WvLog log("closeflushtest");
+    
+    log("Starting.\n");
+    
+    {
+	SillyStream s;
+	
+	s.delay_output(true);
+	s.write("Hello world\n");
+	log("Said hello -- %s\n", s.obu());
+        WVPASS(s.obu() == 12);
+	
+	s.delay_output(false);
+	log("Cancelled delay -- %s\n", s.obu());
+	WVPASS(s.obu() == 12);
+	
+	s.flush_then_close(10000);
+	log("Scheduled close -- %s\n", s.obu());
+	WVPASS(s.obu() == 12);
+        
+	log("Select...\n");
+	s.select(100);
+	
+	log("Destroying -- %s\n", s.obu());
+	WVPASS(s.obu() == 0);
+    }
+    log("Didn't crash.\n");
+}
+#else
+#warning "Skipping old-style section of unittest until it is converted"
+#endif
