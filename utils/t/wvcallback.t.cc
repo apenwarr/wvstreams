4af37c7f18e0a6452ea5fe608237d40a8c908924
mrwise <mrwise@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2003-12-04 20:28:17 +0000

    wvstreams_unittest: added in the code for cbweirdtest which crashes, so it's ifdef'd out for now.  Once that bug is resolved a unittest needs to be written for it.
    BUGZID:2933
    
    
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/wvstreams_unittest@3335 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- utils/t/wvcallback.t.cc
+++ utils/t/wvcallback.t.cc
@@ -2,36 +2,56 @@
 #include "wvcallback.h"
 #include <stdio.h>
 
+// START first old-style definitions
 struct A
-    {
-        int x, y;
-        A(int _x = 0, int _y = 0)
-            { x = _x; y = _y; }
-    
-        A add(const A &a)
-            { return A(x+a.x, y+a.y); }
-    };
+{
+    int x, y;
+    A(int _x = 0, int _y = 0)
+        { x = _x; y = _y; }
+   
+    A add(const A &a)
+        { return A(x+a.x, y+a.y); }
+};
 
-    typedef WvCallback<A, const A &, void *> ACallback;
-    typedef WvCallback<A, const A &> A2Callback;
-    typedef WvCallback<A, void *> A3Callback;
-
-
-    static A bunk(const A &a, void *userdata)
-    {
-        int incr = (int)userdata;
-        return A(a.x+incr, a.y+incr*2);
-    }
+typedef WvCallback<A, const A &, void *> ACallback;
+typedef WvCallback<A, const A &> A2Callback;
+typedef WvCallback<A, void *> A3Callback;
 
 
-    // one-parameter version of bunk()
-    static A bunk1(const A &a)
-    {
-        return bunk(a, (void *)1);
-    }
+static A bunk(const A &a, void *userdata)
+{
+    int incr = (int)userdata;
+    return A(a.x+incr, a.y+incr*2);
+}
+
+
+// one-parameter version of bunk()
+static A bunk1(const A &a)
+{
+    return bunk(a, (void *)1);
+}
+// END first old-style definitions
+
+// START second old-style definitions
+typedef WvCallback<void> Cb;
+
+void f()
+{
+    // nothing
+}
+
+
+class Derived : public Cb
+{
+public:
+    Derived(const Cb &cb) : Cb(cb)
+        { }
+};
+// END second old-style definitions
 
 WVTEST_MAIN("old-style")
 {
+    // first old-style test "callbacktest.cc"
     {
         A a(1000, 2000);
         A result;
@@ -55,5 +75,18 @@ WVTEST_MAIN("old-style")
         
         result = (c3(a));
         if (!WVPASS(result.y == 2 * result.x))
-            printf("   because [%d] != 2 * [%d]\n", result.y, result.x);            }
+            printf("   because [%d] != 2 * [%d]\n", result.y, result.x);
+    }
+    
+#ifdef CBWEIRD_DOESNT_CRASH        
+    //FIXME: Write unittest to correct this bug, once it's known and solved
+    // second old-style test "cbweirdtest.cc"
+    {
+        Cb cb1(f);
+        Cb cb2(cb1);
+        Derived cb3(f);
+        Derived cb4(cb1);
+        Derived cb5(cb3);
+    }
+#endif
 }
