ad112269a1dc0447d5534ffa88c780ce3c60aafd
mrwise <mrwise@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2003-12-10 00:41:54 +0000

    wvstreams_unittest: converted old-style vertest over to unittest
    BUGZID:2933
    
    
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/wvstreams_unittest@3393 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ utils/t/verstring.t.cc
@@ -0,0 +1,36 @@
+/*
+ * Worldvisions Weaver Software:
+ *   Copyright (C) 1997-2002 Net Integration Technologies, Inc.
+ * 
+ * Version number and string manipulations.  Version numbers are 32-bit
+ * hexadecimal numbers such as 0x00012a00.  The first 16 bits are the major
+ * version, and the second 16 bits are the (fractional) minor version.  For
+ * example, the above example corresponds to version "1.2a" (which is the
+ * version string).
+ */
+#include "wvtest.h"
+#include "verstring.h"
+#include <stdio.h>
+
+WVTEST_MAIN("old-style")
+{
+    if (!WVFAIL(strcmp(ver_to_string(0x99998888), "9999.8888")))
+        printf("   because [%s] != [9999.8888]\n", ver_to_string(0x99998888));
+    if (!WVPASS(string_to_ver("1.0") == 0x00010000))
+        printf("   because [%08x] != [00010000]\n", string_to_ver("1.0"));
+    
+    if (!WVFAIL(strcmp(ver_to_string(0x01a00200), "1a0.02")))
+        printf("   because [%s] != [1a0.02]\n", ver_to_string(0x01a00200));
+    if (!WVPASS(string_to_ver(".02a") == 0x000002a0))
+        printf("   because [%08x] != [000002a0]\n", string_to_ver(".02a"));    
+    
+    if (!WVFAIL(strcmp(ver_to_string(0x00001000), "0.1")))
+        printf("   because [%s] != [0.1]\n", ver_to_string(0x00001000));
+    if (!WVPASS(string_to_ver("1b") == 0x001b0000))
+        printf("   because [%08x] != [001b0000]\n", string_to_ver("1b"));    
+    
+    if (!WVFAIL(strcmp(ver_to_string(0x00000000), "0.0")))
+        printf("   because [%s] != [0.0]\n", ver_to_string(0x00000000));
+    if (!WVPASS(string_to_ver("1A.") == 0x001a0000))
+        printf("   because [%08x] != [001a0000]\n", string_to_ver("1A."));
+}
