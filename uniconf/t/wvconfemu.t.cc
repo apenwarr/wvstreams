719dad91ff34eb8c96f4e2522de7e568a86f6b5e
pzion <pzion@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-01-06 18:18:40 +0000

    HEAD: Added wvconfemu unitest that checks that a deleted item in a section no longer appears when you iterate through the section. BUGZID:3153
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@3608 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ uniconf/t/wvconfemu.t.cc
@@ -0,0 +1,48 @@
+#include "wvtest.h"
+#include "wvstring.h"
+#include "wvconfemu.h"
+#include "unitempgen.h"
+
+WVTEST_MAIN("wvconfemu_delete_with_NULL")
+{
+    UniConfGen *unigen = new UniTempGen;
+    UniConfRoot uniconf(unigen);
+    WvConfEmu cfg(uniconf);
+    WvString section = "TestSection", entry = "TestEntry",
+        value = "TestValue", notValue = "NotTestValue";
+    
+    cfg.set(section, entry, value);
+    WVPASS(strcmp(cfg.get(section, entry, notValue), value) == 0);
+
+    cfg.set(section, entry, NULL);
+    WVPASS(strcmp(cfg.get(section, entry, notValue), notValue) == 0);
+
+    WvConfigSectionEmu *sect = cfg[section];
+    WVPASS(sect != NULL);
+
+    WvConfigEntryListEmu::Iter i(*sect);
+    for (i.rewind(); i.next(); )
+        WVFAIL(strcmp(i->name, entry) == 0);
+}
+
+WVTEST_MAIN("wvconfemu_delete_with_empty_string")
+{
+    UniConfGen *unigen = new UniTempGen;
+    UniConfRoot uniconf(unigen);
+    WvConfEmu cfg(uniconf);
+    WvString section = "TestSection", entry = "TestEntry",
+        value = "TestValue", notValue = "NotTestValue";
+    
+    cfg.set(section, entry, value);
+    WVPASS(strcmp(cfg.get(section, entry, notValue), value) == 0);
+
+    cfg.set(section, entry, "");
+    WVPASS(strcmp(cfg.get(section, entry, notValue), notValue) == 0);
+
+    WvConfigSectionEmu *sect = cfg[section];
+    WVPASS(sect != NULL);
+
+    WvConfigEntryListEmu::Iter i(*sect);
+    for (i.rewind(); i.next(); )
+        WVFAIL(strcmp(i->name, entry) == 0);
+}
