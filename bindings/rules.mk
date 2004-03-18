0fd0cf39459b22676bc5cd8a958b0b2885bf1083
jnc <jnc@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-03-18 23:37:12 +0000

    r3_80: Make wvstreams use the SWIG from ports by default
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r3_80@4078 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- bindings/rules.mk
+++ bindings/rules.mk
@@ -1,8 +1,13 @@
 
 GARBAGE+=bindings/uniconf_tcl.c
 
+SWIGPARAMS =
+ifneq ("$(with_swig)x", "x")
+	SWIGPARAMS += -I$(with_swig)/Lib -I$(with_swig)/Lib/tcl
+endif
+
 bindings/libuniconf_tcl.so: bindings/uniconf_tcl.o libuniconf.so -ltcl8.3
 
 bindings/uniconf_tcl.c: include/uniconf.h
-	$(SWIG) -tcl -o $@ $^
+	$(SWIG) $(SWIGPARAMS) -tcl -o $@ $^
 
