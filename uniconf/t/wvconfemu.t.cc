c86e31e88e91ae9beeaab12f7935308ac332e1e5
jnc <jnc@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-06-26 05:34:03 +0000

    r3_80: Add get from nonexistant section to WvConfEmu unit test BUGZID: 6042
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r3_80@5262 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- uniconf/t/wvconfemu.t.cc
+++ uniconf/t/wvconfemu.t.cc
@@ -3,6 +3,24 @@
 #include "wvconfemu.h"
 #include "unitempgen.h"
 
+WVTEST_MAIN("set and get")
+{
+    UniConfGen *unigen = new UniTempGen;
+    UniConfRoot uniconf(unigen);
+    WvConfEmu cfg(uniconf);
+    WvString section = "TestSection", entry = "TestEntry",
+        value = "TestValue", notValue = "NotTestValue",
+        notSection = "No Such Section", notEntry = "No Such Entry";
+
+    // get existing key
+    cfg.set(section, entry, value);
+    WVPASS(strcmp(cfg.get(section, entry, notValue), value) == 0);
+
+    // get nonexistant key
+    WVPASS(strcmp(cfg.get(section, notEntry, notValue), notValue) == 0);
+    WVPASS(strcmp(cfg.get(notSection, entry, notValue), notValue) == 0);
+}
+
 WVTEST_MAIN("delete with empty key name")
 {
     UniConfGen *unigen = new UniTempGen;
