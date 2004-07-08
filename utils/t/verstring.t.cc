227846d298df7b0b7a90dab07c8ded95567734b2
mcote <mcote@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-07-08 19:02:29 +0000

    HEAD: Moved verstring.h to wvverstring.h.  Added new_ver_to_string() and string_to_new_ver() for new version format, plus unit tests.  BUGZID: 6066
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@5463 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- utils/t/verstring.t.cc
+++ utils/t/verstring.t.cc
@@ -9,7 +9,7 @@
  * version string).
  */
 #include "wvtest.h"
-#include "verstring.h"
+#include "wvverstring.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -22,7 +22,7 @@
  * should return a hex number of the form 0xabcdefgh, with implicit
  * meaningless zeros inserted.
  */
-WVTEST_MAIN("vertest.cc")
+WVTEST_MAIN("wvvertest.cc")
 {
     if (!WVFAIL(strcmp(ver_to_string(0x99998888), "9999.8888")))
         printf("   because [%s] != [9999.8888]\n", ver_to_string(0x99998888));
@@ -43,4 +43,39 @@ WVTEST_MAIN("vertest.cc")
         printf("   because [%s] != [0.0]\n", ver_to_string(0x00000000));
     if (!WVPASS(string_to_ver("1A.") == 0x001a0000))
         printf("   because [%08x] != [001a0000]\n", string_to_ver("1A."));
+
+    if (!WVFAIL(strcmp(new_ver_to_string(0x99887777), "99.88.7777")))
+        printf("   because [%s] != [99.88.7777]\n",
+	       new_ver_to_string(0x99887777));
+    if (!WVPASS(string_to_new_ver("1.00.0") == 0x01000000))
+        printf("   because [%08x] != [01000000]\n",
+	       string_to_new_ver("1.00.0"));
+    
+    if (!WVFAIL(strcmp(new_ver_to_string(0x01a00200), "1.a0.02")))
+        printf("   because [%s] != [1.a0.02]\n",
+	       new_ver_to_string(0x01a00200));
+    if (!WVPASS(string_to_new_ver(".02.a") == 0x0002a000))
+        printf("   because [%08x] != [0002a000]\n",
+	       string_to_new_ver(".02.a"));    
+    if (!WVPASS(string_to_new_ver(".5.37ab") == 0x000537ab))
+        printf("   because [%08x] != [000537ab]\n",
+	       string_to_new_ver(".5.37a"));    
+    
+    if (!WVFAIL(strcmp(new_ver_to_string(0x00001000), "0.00.1")))
+        printf("   because [%s] != [0.00.1]\n", new_ver_to_string(0x00001000));
+    if (!WVPASS(string_to_new_ver("1b") == 0x1b000000))
+        printf("   because [%08x] != [1b000000]\n", string_to_new_ver("1b"));
+
+    if (!WVFAIL(strcmp(new_ver_to_string(0x00001000, true), "0.1")))
+	printf("   because [%s] != [0.1]\n", new_ver_to_string(0x00001000,
+							       true));
+    
+    if (!WVFAIL(strcmp(new_ver_to_string(0x00000000), "0.00.0")))
+        printf("   because [%s] != [0.00.0]\n", new_ver_to_string(0x00000000));
+    if (!WVPASS(string_to_new_ver("1A.") == 0x1a000000))
+        printf("   because [%08x] != [1a000000]\n", string_to_new_ver("1A."));
+
+    if (!WVFAIL(strcmp(new_ver_to_string(0x00000000, true), "0.0")))
+        printf("   because [%s] != [0.0]\n", new_ver_to_string(0x00000000,
+							       true));
 }
--- utils/verstring.cc
+++ utils/verstring.cc
@@ -8,31 +8,53 @@
  * example, the above example corresponds to version "1.2a" (which is the
  * version string).
  */
-#include "verstring.h"
+#include "wvverstring.h"
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+
+char *trim_verstr(char *verstr)
+{
+    // trim off trailing zeroes
+    char *cptr;
+
+    for (cptr = strchr(verstr, 0); --cptr >= verstr; )
+    {
+	if (*cptr != '0')
+	    break;
+	
+	if (cptr <= verstr  ||  *(cptr - 1) == '.')
+	    break;
+	
+	*cptr = 0;
+    }
+    return verstr;
+}
 
 const char *ver_to_string(unsigned int ver)
 {
     static char str[10];
     unsigned int maj = (ver & 0xFFFF0000) >> 16, min = (ver & 0x0000FFFF);
-    char *cptr;
     
     sprintf(str, "%x.%04x", maj, min);
-
-    // trim off trailing zeroes from minor number
-    for (cptr = strchr(str, 0); --cptr >= str; )
-    {
-	if (*cptr != '0')
-	    break;
-	
-	if (cptr <= str  ||  *(cptr - 1) == '.')
-	    break;
-	
-	*cptr = 0;
-    }
+    trim_verstr(str);
     
+    return str;
+}
+
+
+const char *new_ver_to_string(unsigned int ver, bool convert_old)
+{
+    static char str[11];
+    unsigned int maj = (ver & 0xFF000000) >> 24, min = (ver & 0x00FF0000) >> 16,
+                 rev = (ver & 0x0000FFFF);
+
+    if (convert_old && !maj)
+	return ver_to_string(ver);
+
+    sprintf(str, "%x.%02x.%04x", maj, min, rev);
+    trim_verstr(str);
+
     return str;
 }
 
@@ -48,6 +70,44 @@ unsigned int string_to_ver(const char *str)
     cptr = (unsigned char *)str;
     for (; *cptr && *cptr != '.' && *cptr != '_'; cptr++)
     {
+        idx = (unsigned char *)strchr(lookup, tolower(*cptr));
+        if (!idx)
+            continue;
+        
+        maj = (maj << 4) | ((char *)idx - lookup);
+    }
+    
+    // do the minor number
+    for (bits = 4; *cptr && bits > 0; cptr++)
+    {
+        idx = (unsigned char *)strchr(lookup, tolower(*cptr));
+        if (!idx)
+            continue;
+        
+        min = (min << 4) | ((char *)idx - lookup);
+        bits--;
+    }
+    
+    return (maj << 16) | (min << (4*bits));
+}
+
+
+unsigned int string_to_new_ver(const char *str)
+{
+    static char lookup[] = "0123456789abcdef";
+    unsigned int maj = 0, min = 0, rev = 0;
+    unsigned char *cptr, *idx;
+    int bits;
+    
+    // do the major number
+    cptr = (unsigned char *)str;
+    for (; *cptr; cptr++)
+    {
+	if (*cptr == '.' || *cptr == '_')
+	{
+	    cptr++;
+	    break;
+	}
 	idx = (unsigned char *)strchr(lookup, tolower(*cptr));
 	if (!idx)
 	    continue;
@@ -56,15 +116,25 @@ unsigned int string_to_ver(const char *str)
     }
     
     // do the minor number
-    for (bits = 4; *cptr && bits > 0; cptr++)
+    for (; *cptr && *cptr != '.' && *cptr != '_'; cptr++)
     {
 	idx = (unsigned char *)strchr(lookup, tolower(*cptr));
 	if (!idx)
 	    continue;
 	
 	min = (min << 4) | ((char *)idx - lookup);
-	bits--;
     }
     
-    return (maj << 16) | (min << (4*bits));
+    // do the revision number
+    for (bits = 4; *cptr && bits > 0; cptr++)
+    {
+	idx = (unsigned char *)strchr(lookup, tolower(*cptr));
+	if (!idx)
+	    continue;
+
+	rev = (rev << 4) | ((char *)idx - lookup);
+	bits--;
+    }
+
+    return (maj << 24) | (min << 16) | (rev << (4*bits));
 }
