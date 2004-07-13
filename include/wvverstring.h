5a470066e1ebd775f61a0b29c377e1e7ab30dd49
mcote <mcote@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-07-13 23:26:30 +0000

    r3_80_mcote (versioning bridge): Ported & modified verstring changes from HEAD.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r3_80_mcote@5625 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- include/wvverstring.h
+++ include/wvverstring.h
@@ -3,30 +3,48 @@
  *   Copyright (C) 1997-2004 Net Integration Technologies, Inc.
  *
  * Version number and string manipulations.
+ *
+ * The old version number was a 32-bit hexadecimal number, split into a 16-bit
+ * major version and a 16-bit minor version.  For example, the old-style string
+ * equivalent of 0x00012a00 would be "1.2a".
+ *
+ * The new-style version number is a 32-bit hexadecimal number, split into
+ * an 8-bit major version, an 8-bit minor version, and a 16-bit revision
+ * number.  The new-style string equivalent of 0x01020150 would be "1.02.0150".
  */
 #ifndef __WVVERSTRING_H
 #define __WVVERSTRING_H
 
+/** Watershed for version style, given in new style.
+ * Any version equal to or greater than 'wvversion_watershed' will be treated
+ * as a new-style version (set to 0 to always use the new style).
+ * Versions less than 'version_watershed' will use the old style.
+ * The default value is 0.
+ */
+extern unsigned int wvversion_watershed;
+
+/** Converts a version number to a c string.
+ * Set 'my_watershed' to non-zero to override 'wvversion_watershed'.
+ */
+const char *ver_to_string(unsigned int ver, unsigned int my_watershed = 0);
+
 /** Converts an old-style version number to a c string.
- * The version number is a 32-bit hexadecimal number, split into a 16-bit
- * major version and a 16-bit minor version.  The string equivalent of 
- * 0x00012a00 is "1.2a".
+ * This is used automatically by ver_to_string() if ver < watershed.
  */
-const char *ver_to_string(unsigned int ver);
+const char *old_ver_to_string(unsigned int ver);
 
-/** Converts a new-style version number to a c string.
- * The new-style version number is a 32-bit hexadecimal number, split into
- * an 8-bit major version, an 8-bit minor version, and a 16-bit revision
- * number.  The string equivalent of 0x01020150 would be "1.02.0150".
- * If 'convert_old' is true, any version number starting with 00 will be
- * converted to an old-style version string (see ver_to_string()).
+/** Converts a c string to a version number.
+ * Set 'my_watershed' to non-zero to override 'wvversion_watershed'.
  */
-const char *new_ver_to_string(unsigned int ver, bool convert_old = false);
+unsigned int string_to_ver(const char *str, unsigned int my_watershed = 0);
 
-/// Converts a c string to an old-style version number.
-unsigned int string_to_ver(const char *str);
-
-/// Converts a c string to a new-style version number.
-unsigned int string_to_new_ver(const char *str);
+/** Converts a c string to a new-style version number.
+ * This is used automatically by string_to_ver() if string_to_ver(str) <
+ * watershed.
+ * This function can also understand missing zeros, which are assumed to be
+ * at the front for the major and minor versions and at the end for the
+ * revision.  Thus "1.2.15" would be translated as 0x01021500.
+ */
+unsigned int string_to_old_ver(const char *str);
 
 #endif // __WVVERSTRING_H
--- utils/verstring.cc
+++ utils/verstring.cc
@@ -8,36 +8,60 @@
  * example, the above example corresponds to version "1.2a" (which is the
  * version string).
  */
-#include "verstring.h"
+#include "wvverstring.h"
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 
-const char *ver_to_string(unsigned int ver)
-{
-    static char str[10];
-    unsigned int maj = (ver & 0xFFFF0000) >> 16, min = (ver & 0x0000FFFF);
-    char *cptr;
-    
-    sprintf(str, "%x.%04x", maj, min);
+unsigned int wvversion_watershed = 0;
 
-    // trim off trailing zeroes from minor number
-    for (cptr = strchr(str, 0); --cptr >= str; )
+char *trim_verstr(char *verstr)
+{
+    // trim off trailing zeroes
+    char *cptr;
+
+    for (cptr = strchr(verstr, 0); --cptr >= verstr; )
     {
 	if (*cptr != '0')
 	    break;
 	
-	if (cptr <= str  ||  *(cptr - 1) == '.')
+	if (cptr <= verstr  ||  *(cptr - 1) == '.')
 	    break;
 	
 	*cptr = 0;
     }
+    return verstr;
+}
+
+const char *old_ver_to_string(unsigned int ver)
+{
+    static char str[10];
+    unsigned int maj = (ver & 0xFFFF0000) >> 16, min = (ver & 0x0000FFFF);
+
+    sprintf(str, "%x.%04x", maj, min);
+    trim_verstr(str);
     
     return str;
 }
 
 
-unsigned int string_to_ver(const char *str)
+const char *ver_to_string(unsigned int ver, unsigned int my_watershed)
+{
+    static char str[11];
+    unsigned int maj = (ver & 0xFF000000) >> 24, min = (ver & 0x00FF0000) >> 16,
+                 rev = (ver & 0x0000FFFF);
+
+    if ((my_watershed && ver < my_watershed) || (!my_watershed && ver < wvversion_watershed))
+	return old_ver_to_string(ver);
+
+    sprintf(str, "%x.%02x.%04x", maj, min, rev);
+    trim_verstr(str);
+
+    return str;
+}
+
+
+unsigned int string_to_old_ver(const char *str)
 {
     static char lookup[] = "0123456789abcdef";
     unsigned int maj = 0, min = 0;
@@ -48,6 +72,44 @@ unsigned int string_to_ver(const char *str)
     cptr = (unsigned char *)str;
     for (; *cptr && *cptr != '.' && *cptr != '_'; cptr++)
     {
+        idx = (unsigned char *)strchr(lookup, tolower(*cptr));
+        if (!idx)
+            continue;
+        
+        maj = (maj << 4) | ((char *)idx - lookup);
+    }
+    
+    // do the minor number
+    for (bits = 4; *cptr && bits > 0; cptr++)
+    {
+        idx = (unsigned char *)strchr(lookup, tolower(*cptr));
+        if (!idx)
+            continue;
+        
+        min = (min << 4) | ((char *)idx - lookup);
+        bits--;
+    }
+    
+    return (maj << 16) | (min << (4*bits));
+}
+
+
+unsigned int string_to_ver(const char *str, unsigned int my_watershed)
+{
+    static char lookup[] = "0123456789abcdef";
+    unsigned int maj = 0, min = 0, rev = 0, ver;
+    unsigned char *cptr, *idx;
+    int bits;
+
+    // do the major number
+    cptr = (unsigned char *)str;
+    for (; *cptr; cptr++)
+    {
+	if (*cptr == '.' || *cptr == '_')
+	{
+	    cptr++;
+	    break;
+	}
 	idx = (unsigned char *)strchr(lookup, tolower(*cptr));
 	if (!idx)
 	    continue;
@@ -56,7 +118,7 @@ unsigned int string_to_ver(const char *str)
     }
     
     // do the minor number
-    for (bits = 4; *cptr && bits > 0; cptr++)
+    for (bits = 2; *cptr && *cptr != '.' && *cptr != '_' && bits > 0; cptr++)
     {
 	idx = (unsigned char *)strchr(lookup, tolower(*cptr));
 	if (!idx)
@@ -66,5 +128,21 @@ unsigned int string_to_ver(const char *str)
 	bits--;
     }
     
-    return (maj << 16) | (min << (4*bits));
+    // do the revision number
+    for (bits = 4; *cptr && bits > 0; cptr++)
+    {
+	idx = (unsigned char *)strchr(lookup, tolower(*cptr));
+	if (!idx)
+	    continue;
+
+	rev = (rev << 4) | ((char *)idx - lookup);
+	bits--;
+    }
+
+    ver = (maj << 24) | (min << 16) | (rev << (4*bits));
+
+    if ((my_watershed && ver < my_watershed) || (!my_watershed && ver < wvversion_watershed))
+	return string_to_old_ver(str);
+
+    return ver;
 }
