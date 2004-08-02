04a4ff969af4679b2ffb8c0f0525cbeea886d47b
pphaneuf <pphaneuf@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-08-02 22:02:01 +0000

    Removed usage of uses_continue_select and replaced them with explicit uses of WvCont. BUGZID:6101
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@6155 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- ipstreams/tests/contseltest.cc
+++ ipstreams/tests/contseltest.cc
@@ -9,15 +9,10 @@
 #include "wvistreamlist.h"
 #include "wvlog.h"
 #include "strutils.h"
+#include "wvcont.h"
 
-static void stream_call(WvStream &s, void *userdata)
+static void *stream_call(WvStream& s, void* contdata = 0)
 {
-    if (!s.uses_continue_select)
-    {
-	s.uses_continue_select = true;
-	return;
-    }
-    
     char *line;
     static int sc_count = 0;
     int mynum = ++sc_count, count = 0;
@@ -40,6 +35,22 @@ static void stream_call(WvStream &s, void *userdata)
 	count++;
 	s.continue_select(100*mynum);
     }
+
+    return 0;
+}
+
+
+static void cont_call(WvCont cont, WvStream &s, void* userdata)
+{
+    cont();
+}
+
+
+static void setupcont_call(WvStream &s, void* userdata)
+{
+    WvCont cont(WvBoundCallback<WvCallback<void*, void*>, WvStream&>(stream_call, s));
+
+    s.setcallback(WvBoundCallback<WvStreamCallback, WvCont>(cont_call, cont), NULL);
 }
 
 
@@ -49,10 +60,10 @@ int main()
     WvIStreamList l;
     
     WvTCPListener listen(WvIPPortAddr("0.0.0.0:1129"));
-    listen.auto_accept(&l, stream_call, NULL);
-    
-    wvcon->setcallback(stream_call, NULL);
-    
+    listen.auto_accept(&l, setupcont_call, NULL);
+
+    wvcon->setcallback(setupcont_call, NULL);
+
     log("Listening on port %s\n", *listen.src());
     
     l.append(&listen, false);
