a9f88517c9a7c61d46aaf8b453cfee096c449f5d
slajoie <slajoie@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-08-11 17:11:39 +0000

    Updated WvStreams/Win32 to latest changes.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@6449 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- Win32WvStreams/libwvutils/wvwin32task.cc
+++ Win32WvStreams/libwvutils/wvwin32task.cc
@@ -147,7 +147,7 @@ WvTask *WvTaskMan::start(WvStringParm name,
 	if (i().stacksize >= stacksize)
 	{
 	    t = &i();
-	    i.link->auto_free = false;
+	    i.link->set_autofree(false);
 	    i.unlink();
 	    t->recycled = false;
 	    t->start(name, func, userdata);
