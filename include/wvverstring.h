0a4e248f3cb5a82ec4474e8840172b8e085818de
mcote <mcote@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-08-11 00:56:02 +0000

    r3_80_mcote: Got rid of watershed stuff, replaced by functions that make arbitrary judgements about the style of version numbers/strings.  BUGZID: 6066
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r3_80_mcote@6438 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- include/wvverstring.h
+++ include/wvverstring.h
@@ -15,39 +15,47 @@
 #ifndef __WVVERSTRING_H
 #define __WVVERSTRING_H
 
-/** Watershed for version style, given in new style.
- * Any version equal to or greater than 'wvversion_watershed' will be treated
- * as a new-style version (set to 0 to always use the new style).
- * Versions less than 'version_watershed' will use the old style.
- * The default value is 0.
+/** Converts a version number to a C string.
+ * This uses is_new_ver() to determine which style to use for the resulting
+ * string.
  */
-extern unsigned int wvversion_watershed;
+const char *ver_to_string(unsigned int ver);
 
-/** Converts a version number to a c string.
- * Set 'my_watershed' to non-zero to override 'wvversion_watershed'.
- */
-const char *ver_to_string(unsigned int ver, unsigned int my_watershed = 0);
-
-/** Converts an old-style version number to a c string.
- * This is used automatically by ver_to_string() if ver < watershed.
- */
+/// Converts a version number to a C string using the old style.
 const char *old_ver_to_string(unsigned int ver);
 
-/** Converts a c string to a version number.
- * Set 'my_watershed' to non-zero to override 'wvversion_watershed'.
- */
-unsigned int string_to_ver(const char *str, unsigned int my_watershed = 0);
+/// Converts a version number to a C string using the new sytle.
+const char *new_ver_to_string(unsigned int ver);
 
-/** Converts a c string to a new-style version number.
- * This is used automatically by string_to_ver() if string_to_ver(str) <
- * watershed.
- * This function can also understand missing zeros, which are assumed to be
- * at the front for the major and minor versions and at the end for the
- * revision.  Thus "1.2.15" would be translated as 0x01021500.
+/** Converts a C string to a version number.
+ * This uses is_new_verstr() to determine which style to use for the resulting
+ * version integer.
  */
+unsigned int string_to_ver(const char *str);
+
+/// Converts a C string to a version number using the old style.
 unsigned int string_to_old_ver(const char *str);
 
-/// Trims zeroes off the end of a version string.
+/// Converts a C string to a version number using the new style.
+unsigned int string_to_new_ver(const char *str);
+
+/** Returns true if 'ver' is in the new style.
+ * If the first two hex digits are non-zero, it is the old style; otherwise,
+ * it's the new style.  This somewhat arbitrary distinction was made to ease
+ * migration of some WvStreams projects.  Use the specific new and old
+ * functions if you don't like this.
+ */
+bool is_new_ver(unsigned int ver);
+
+/** Returns true if 'str' is in the new style.
+ * If there are 2 periods ('.') in the string, it is the new style; otherwise,
+ * it's the old style.
+ */
+bool is_new_verstr(const char *str);
+
+/** Trims zeroes off the end of a version string.
+ * Used by old-style functions only.
+ */
 char *trim_verstr(char *verstr);
 
 #endif // __WVVERSTRING_H
