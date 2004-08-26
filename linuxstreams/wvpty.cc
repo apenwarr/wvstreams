128bb39d349efa7f57c69abaff977b90a31fc629
(no author) <(no author)@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-08-26 15:37:08 +0000

    This commit was manufactured by cvs2svn to create branch 'wvdiskd'.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/wvdiskd@6881 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ linuxstreams/wvpty.cc
@@ -0,0 +1,158 @@
+/* -*- Mode: C++ -*-
+ * Worldvisions Weaver Software:
+ *   Copyright (C) 1997-2004 Net Integration Technologies, Inc.
+ *  
+ * WvStreams implementation of ptys under Linux.
+ *
+ * For more information on programming ptys, see chapter 19 of
+ * Stevens' "Advanced Programming in the UNIX Environment"
+ */
+
+#include "wvpty.h"
+
+#include <grp.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+bool WvPty::open_master()
+{
+    const char *xvals = "pqrstuvwxyzPQRST";
+    const char *yvals = "0123456789abcdef";
+    char pty[] = "/dev/ptyXY";
+
+    for (int i=0; xvals[i]; ++i)
+    {
+        pty[8] = xvals[i];
+
+        for (int j=0; yvals[j]; ++j)
+        {
+            pty[9] = yvals[j];
+
+            int fd = ::open(pty, O_RDWR);
+            if (fd < 0)
+            {
+                if (errno == ENOENT)
+                    return false; // no more ptys
+            }
+            else
+            {
+                setfd(fd);
+
+                _master = pty;
+                _master.unique();
+
+                pty[5] = 't';
+                _slave = pty;
+                _slave.unique();
+
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+bool WvPty::open_slave()
+{
+    // try to change owner and permissions.  this will only work if we 
+    // are root; if we're not root, we don't care.
+    struct group *gr = ::getgrnam("tty");
+    ::chown(_slave, ::getuid(), gr? gr->gr_gid: (gid_t)-1);
+
+    ::chmod(_slave, S_IRUSR | S_IWUSR | S_IWGRP);
+
+    setfd(::open(_slave, O_RDWR));
+    
+    return getfd() != -1;
+}
+
+WvPty::WvPty(const char *program, const char * const *argv)
+        : _pid(-1), _exit_status(242)
+{
+    if (!open_master()
+            || (_pid = ::fork()) < 0)
+    {
+        // error
+        _pid = -1;
+        setfd(-1);
+    }
+    else if (_pid == 0)
+    {
+        // child
+        int fd = getfd();
+        if (::close(fd) < 0
+                || ::setsid() < 0
+                || !open_slave()
+                || ::ioctl(getrfd(), TIOCSCTTY, NULL)
+                || ::dup2(getrfd(), STDIN_FILENO) < 0
+                || ::dup2(getwfd(), STDOUT_FILENO) < 0
+                || ::dup2(getwfd(), STDERR_FILENO) < 0
+                || (getfd() > STDERR_FILENO && ::close(getfd()) < 0))
+            goto _error;
+       
+        setfd(-1);
+        execvp(program, (char * const *)argv);
+
+_error:
+        setfd(-1);
+        _exit(242);
+    }
+}
+
+void WvPty::kill(int signum)
+{
+    if (_pid != -1)
+	::kill(_pid, signum);
+}
+
+void WvPty::monitor_child(bool wait)
+{
+    if (_pid != -1)
+    {
+        int status;
+        if (waitpid(_pid, &status, wait? 0: WNOHANG) == _pid)
+        {
+            _pid = -1;
+            _exit_status = status;
+        }
+    }
+}
+
+bool WvPty::child_exited()
+{
+    monitor_child(false);
+    return _pid == -1;     
+}
+
+bool WvPty::child_killed()
+{
+    monitor_child(false);
+    return _pid == -1 && WIFSIGNALED(_exit_status);
+}
+
+int WvPty::finish()
+{
+    monitor_child(true);
+    return WEXITSTATUS(_exit_status);
+}
+
+int WvPty::exit_status()
+{
+    monitor_child(false);
+    if (_pid == -1)
+    {
+        if (child_killed())
+            return WTERMSIG(_exit_status);
+        else
+            return WEXITSTATUS(_exit_status);
+    }
+    else
+        return 242;
+}
+
---
+++ streams/wvstreamsdaemon.cc
@@ -0,0 +1,107 @@
+/* -*- Mode: C++ -*-
+ * Worldvisions Tunnel Vision Software:
+ *   Copyright (C) 1997-2002 Net Integration Technologies, Inc.
+ *
+ * High-level abstraction for creating daemon processes that do
+ * nothing but listen on a list of WvStreams and add connections
+ * to the global list.
+ */
+
+#include "wvstreamsdaemon.h"
+
+#include <signal.h>
+
+WvStreamsDaemon::WvStreamsDaemon(WvStringParm name, WvStringParm version,
+        WvStreamsDaemonCallback cb, void *ud)
+    : WvDaemon(name, version,
+                WvDaemonCallback(this, &WvStreamsDaemon::start_cb),
+                WvDaemonCallback(this, &WvStreamsDaemon::run_cb),
+                WvDaemonCallback(this, &WvStreamsDaemon::stop_cb)),
+                callback(cb), userdata(ud)
+{
+    signal(SIGPIPE, SIG_IGN);
+}
+
+void WvStreamsDaemon::start_cb(WvDaemon &daemon, void *)
+{
+    callback(*this, userdata);
+}
+
+void WvStreamsDaemon::run_cb(WvDaemon &daemon, void *)
+{
+    if (streams.count() == 0)
+    {
+        log(WvLog::Error, "No streams; exiting\n");
+        die();
+    }
+
+    while (daemon.should_run())
+        WvIStreamList::globallist.runonce();
+}
+
+void WvStreamsDaemon::stop_cb(WvDaemon &daemon, void *)
+{
+    streams.zap();
+}
+
+void WvStreamsDaemon::stop_full_close_cb(WvDaemon &daemon, void *)
+{
+    WvIStreamList::globallist.zap();
+    streams.zap();
+}
+
+void WvStreamsDaemon::add_stream(IWvStream *istream,
+    	bool auto_free, const char *id)
+{
+    streams.append(istream, false);
+    WvIStreamList::globallist.append(istream, auto_free);
+}
+
+void WvStreamsDaemon::add_restart_stream(IWvStream *istream,
+    	bool auto_free, const char *id)
+{
+    add_stream(istream, auto_free, id);
+    
+    istream->setclosecallback(
+            WvStreamCallback(this, &WvStreamsDaemon::restart_close_cb),
+                    (void *)id);
+}
+
+void WvStreamsDaemon::add_die_stream(IWvStream *istream,
+    	bool auto_free, const char *id)
+{
+    add_stream(istream, auto_free, id);
+    
+    istream->setclosecallback(
+            WvStreamCallback(this, &WvStreamsDaemon::die_close_cb), 
+                    (void *)id);
+}
+
+void WvStreamsDaemon::close_existing_connections_on_restart()
+{
+    stop_callback =
+        WvDaemonCallback(this, &WvStreamsDaemon::stop_full_close_cb);
+}
+
+void WvStreamsDaemon::restart_close_cb(WvStream &, void *ud)
+{
+    if (should_run())
+    {
+        const char *id = (const char *)ud;
+        log(WvLog::Error, "%s is stale; restarting\n",
+                id? id: "Stream");
+        restart();
+    }
+}
+
+void WvStreamsDaemon::die_close_cb(WvStream &, void *ud)
+{
+    if (should_run())
+    {
+        const char *id = (const char *)ud;
+        log(WvLog::Error, "%s is stale; dying\n",
+                id? id: "Stream");
+        die();
+    }
+}
+
