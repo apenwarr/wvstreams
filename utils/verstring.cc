ea52e3126d921ac57cd6e8c1b5722585ea901079
mcote <mcote@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-09-10 22:55:20 +0000

    r3_80: Merging verstring changes from r3_80_mcote. BUGZID: 7271
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r3_80@7091 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- utils/verstring.cc
+++ utils/verstring.cc
@@ -8,36 +8,45 @@
  * example, the above example corresponds to version "1.2a" (which is the
  * version string).
  */
-#include "verstring.h"
+#include "wvverstring.h"
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 
-const char *ver_to_string(unsigned int ver)
+const char *old_ver_to_string(unsigned int ver)
 {
     static char str[10];
     unsigned int maj = (ver & 0xFFFF0000) >> 16, min = (ver & 0x0000FFFF);
-    char *cptr;
-    
-    sprintf(str, "%x.%04x", maj, min);
 
-    // trim off trailing zeroes from minor number
-    for (cptr = strchr(str, 0); --cptr >= str; )
-    {
-	if (*cptr != '0')
-	    break;
-	
-	if (cptr <= str  ||  *(cptr - 1) == '.')
-	    break;
-	
-	*cptr = 0;
-    }
+    sprintf(str, "%x.%04x", maj, min);
+    trim_verstr(str);
     
     return str;
 }
 
 
-unsigned int string_to_ver(const char *str)
+const char *new_ver_to_string(unsigned int ver)
+{
+    static char str[11];
+    unsigned int maj = (ver & 0xFF000000) >> 24, min = (ver & 0x00FF0000) >> 16,
+                 rev = (ver & 0x0000FFFF);
+
+    sprintf(str, "%x.%02x.%04x", maj, min, rev);
+
+    return str;
+}
+
+
+const char *ver_to_string(unsigned int ver)
+{
+    if (is_new_ver(ver))
+        return new_ver_to_string(ver);
+
+    return old_ver_to_string(ver);
+}
+
+
+unsigned int string_to_old_ver(const char *str)
 {
     static char lookup[] = "0123456789abcdef";
     unsigned int maj = 0, min = 0;
@@ -48,6 +57,44 @@ unsigned int string_to_ver(const char *str)
     cptr = (unsigned char *)str;
     for (; *cptr && *cptr != '.' && *cptr != '_'; cptr++)
     {
+        idx = (unsigned char *)strchr(lookup, tolower(*cptr));
+        if (!idx)
+            continue;
+        
+        maj = (maj << 4) | ((char *)idx - lookup);
+    }
+    
+    // do the minor number
+    for (bits = 4; *cptr && bits > 0; cptr++)
+    {
+        idx = (unsigned char *)strchr(lookup, tolower(*cptr));
+        if (!idx)
+            continue;
+        
+        min = (min << 4) | ((char *)idx - lookup);
+        bits--;
+    }
+    
+    return (maj << 16) | (min << (4*bits));
+}
+
+
+unsigned int string_to_new_ver(const char *str)
+{
+    static char lookup[] = "0123456789abcdef";
+    unsigned int maj = 0, min = 0, rev = 0, ver;
+    unsigned char *cptr, *idx;
+    int bits;
+
+    // do the major number
+    cptr = (unsigned char *)str;
+    for (; *cptr; cptr++)
+    {
+	if (*cptr == '.' || *cptr == '_')
+	{
+	    cptr++;
+	    break;
+	}
 	idx = (unsigned char *)strchr(lookup, tolower(*cptr));
 	if (!idx)
 	    continue;
@@ -56,7 +103,7 @@ unsigned int string_to_ver(const char *str)
     }
     
     // do the minor number
-    for (bits = 4; *cptr && bits > 0; cptr++)
+    for (bits = 2; *cptr && *cptr != '.' && *cptr != '_' && bits > 0; cptr++)
     {
 	idx = (unsigned char *)strchr(lookup, tolower(*cptr));
 	if (!idx)
@@ -66,5 +113,62 @@ unsigned int string_to_ver(const char *str)
 	bits--;
     }
     
-    return (maj << 16) | (min << (4*bits));
+    // do the revision number
+    for (bits = 4; *cptr && bits > 0; cptr++)
+    {
+	idx = (unsigned char *)strchr(lookup, tolower(*cptr));
+	if (!idx)
+	    continue;
+
+	rev = (rev << 4) | ((char *)idx - lookup);
+	bits--;
+    }
+
+    ver = (maj << 24) | (min << 16) | (rev << (4*bits));
+
+    return ver;
+}
+
+
+unsigned int string_to_ver(const char *str)
+{
+    if (is_new_verstr(str))
+        return string_to_new_ver(str);
+
+    return string_to_old_ver(str);
+}
+
+
+bool is_new_ver(unsigned int ver)
+{
+    return (ver & 0xff000000);
+}
+
+
+bool is_new_verstr(const char *str)
+{
+    char *p = strchr(str, '.');
+    if (p && strchr(p+1, '.'))
+        return true;
+
+    return false;
+}
+
+
+char *trim_verstr(char *verstr)
+{
+    // trim off trailing zeroes
+    char *cptr;
+
+    for (cptr = strchr(verstr, 0); --cptr >= verstr; )
+    {
+	if (*cptr != '0')
+	    break;
+	
+	if (cptr <= verstr  ||  *(cptr - 1) == '.')
+	    break;
+	
+	*cptr = 0;
+    }
+    return verstr;
 }
