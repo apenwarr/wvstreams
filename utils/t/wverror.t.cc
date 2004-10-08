ab27654b7f63fe31cfcb05af5fdb62859d463cd9
mtsai <mtsai@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-10-08 17:04:23 +0000

    BUGZID: 3854 Hopefully that is sufficient.. =)
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r3_80@7585 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- utils/t/wverror.t.cc
+++ utils/t/wverror.t.cc
@@ -1,145 +1,29 @@
 #include "wvtest.h"
 #include "wverror.h"
+#include "wvstring.h"
 
-
-void testnoerr(const WvErrorBase &e)
+WVTEST_MAIN("Basic error test")
 {
-    WVPASS(e.isok());
-    WVPASS(e.geterr() == 0);
-    WVPASSEQ(e.errstr(), strerror(0));
-}
-
-
-void testerr(const WvErrorBase &e, int err)
-{
-    WVPASS(!e.isok());
-    WVPASS(e.geterr() == err);
-    WVPASSEQ(e.errstr(), strerror(err));
-}
-
-
-void testerrstr(const WvErrorBase &e, WvStringParm err)
-{
-    WVPASS(!e.isok());
-    WVPASS(e.geterr() == -1);
-    WVPASSEQ(e.errstr(), err);
-}
-
-
-WVTEST_MAIN("simple set")
-{
-    WvError e;
-
-    // initial state
-    testnoerr(e);
-
-    // set an int
-    e.seterr(1);
-    testerr(e, 1);
-
-    // int not overwritten
-    e.seterr(2);
-    testerr(e, 1);
-
-    e.seterr("special");
-    testerr(e, 1);
-
-    // reset to initial
-    e.noerr();
-    testnoerr(e);
-
-    // set a string
-    e.seterr("special");
-    testerrstr(e, "special");
-
-    // string not overwritten
-    e.seterr(2);
-    testerrstr(e, "special");
-
-    e.seterr("specialer");
-    testerrstr(e, "special");
-}
-
-WVTEST_MAIN("copy WvErrorBase")
-{
-    WvError e1;
-    WvError e2;
-   
-    // initial over empty 
-    e1.seterr(e2);
-    testnoerr(e1);
-
-    // initial over int
-    e1.seterr(1);
-    e1.seterr(e2);
-    testerr(e1, 1);
-    e1.noerr();
+    WvError testerr;
+    testerr.set("This is a special error.");
+    WVPASS(testerr.str() == "This is a special error.");
     
-    // initial over str
-    e1.seterr("special");
-    e1.seterr(e2);
-    testerrstr(e1, "special");
-    e1.noerr();
+    testerr.set(0);     // Should not change the error.
+    WVFAIL(testerr.str() == "Success");
 
-    e2.seterr(2);
+    testerr.reset();    // Actually resetting the error to default.
+    WVPASS(testerr.str() == "Success");
+
+    WvString prev(testerr.str());
+
+    // Just going through first 10 error codes..
+    // for some reason.. or no reason at all.. =/
+    for (int i=1; i<10; i++)
+    {
+        testerr.set(i);
+        WVFAIL(prev == testerr.str()); 
+        prev = testerr.str();
+        testerr.reset();
+    }
     
-    // int over empty 
-    e1.seterr(e2);
-    testerr(e1, 2);
-    e1.noerr();
-    
-    // int over int
-    e1.seterr(1);
-    e1.seterr(e2);
-    testerr(e1, 1);
-    e1.noerr();
-
-    // int over str
-    e1.seterr("special");
-    e1.seterr(e2);
-    testerrstr(e1, "special");
-    e1.noerr();
-
-    e2.noerr();
-    e2.seterr("specialer");
-
-    // str over empty
-    e1.seterr(e2);
-    testerrstr(e1, "specialer");
-    e1.noerr();
-
-    // str over int
-    e1.seterr(1);
-    e1.seterr(e2);
-    testerr(e1, 1);
-    e1.noerr();
-
-    // str over str
-    e1.seterr("special");
-    e1.seterr(e2);
-    testerrstr(e1, "special");
-    e1.noerr();
-}
-
-WVTEST_MAIN("set to -1")
-{
-    WvError e;
-
-    // overwrite an int
-    e.seterr(1);
-    e.seterr(-1);
-    testerr(e, 1);
-    e.noerr();
-    
-    // overwrite a str
-    e.seterr("special");
-    e.seterr(-1);
-    testerrstr(e, "special");
-    e.noerr();
-
-#if 0
-    // set directly - should crash
-    e.seterr(-1);
-    testnoerr(e);
-#endif
 }
