ba3bd6f33a1753252969c9928793755adcc378e0
wlach <wlach@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-11-05 01:00:44 +0000

    r4_1: Add a (ifdefed out for now) unit test which demonstrates two different types of bad behaviour when proxying between two uniconf daemons, all in an effort to fix.. BUGZID:8552
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r4_1@7933 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- uniconf/t/uniconfd.t.cc
+++ uniconf/t/uniconfd.t.cc
@@ -1,15 +1,91 @@
 #include "wvtest.h"
 #include "uniconfdaemon.h"
-#include "wvstringlist.h"
-#include "wvtclstring.h"
 #include "uniconfroot.h"
-#include "wvtcp.h"
+#include "unitempgen.h"
+#include "wvstringlist.h"
 #include "wvistreamlist.h"
+#include "wvunixsocket.h"
+#include "wvstreamclone.h"
+#include "wvlog.h"
+#include "wvtclstring.h"
+#include "wvtcp.h"
+#include "wvfile.h"
+#include "wvpipe.h"
+#include "wvstringlist.h"
 #include <signal.h>
 
+/**** Generic daemon testing helpers ****/
 
-static void spin(WvIStreamList &l)
+DeclareWvList(WvStringList);
+
+class UniConfDaemonClientConn : public WvStreamClone
 {
+public:
+    UniConfDaemonClientConn(IWvStream *s, WvStringList *_commands, 
+                            WvStringListList *_expected_responses) :        
+        WvStreamClone(s),
+        commands(_commands),
+        expected_responses(_expected_responses),
+        log("UniConfDaemonClientConn", WvLog::Debug)      
+        {
+            setclone(s);
+            uses_continue_select = true;
+        }
+    virtual ~UniConfDaemonClientConn() 
+        {
+            log("Destructing\n");
+            terminate_continue_select();            
+        }
+
+    virtual void close()
+        {
+            log("Closing connection\n");
+            cloned->close();
+        }
+
+    virtual void execute() 
+        {
+            WvStreamClone::execute();
+            
+            // order is always process responses to last command (or conn),
+            // then send the next command
+            // if we're out of expected responses, that means we're done
+            if (expected_responses->count())
+            {
+                while (expected_responses->first()->count())
+                {
+                    WvString line = getline(-1);
+                    WvString expected = expected_responses->first()->popstr();
+                    log(">> '%s' (expect: '%s')\n", line, expected);
+                    WVPASS(!strcmp(line.cstr(), expected.cstr()));
+                } 
+                expected_responses->unlink_first();
+                
+                if (commands->count() > 0)
+                {
+                    WvString command = commands->popstr();
+                    print("%s\n", command);
+                    log("<< %s\n", command);
+                }
+            }
+
+            // expecting nothing more, can close
+            if (!expected_responses->count())
+            {
+                close();
+            }
+        }
+
+private:
+    WvStringList *commands;
+    WvStringListList *expected_responses;
+    WvLog log;
+};
+
+/**** Daemon surprise close test and helpers ****/
+#if 0
+static void spin(WvIStreamList &l)
+{   
     int max;
     for (max = 0; max < 100 && l.select(10); max++)
     {
@@ -18,8 +94,8 @@ static void spin(WvIStreamList &l)
     }
     wvcon->print("\n");
     WVPASS(max < 100);
+    
 }
-
 
 static void appendbuf(WvStream &s, void *_buf)
 {
@@ -90,3 +166,196 @@ WVTEST_MAIN("daemon surprise close")
     //linecmp(l, buf, "NOTICE", "x", "");
     //linecmp(l, buf, "NOTICE", "x/y", "z");
 }
+#endif
+/**** Daemon quit test ****/
+
+// sort of useless: this functionality already exists in
+// daemon/tests and is automatically exercised. furthermore, we only
+// test that the server responds with 'OK' upon close (rather than
+// actually testing whether or not it closed). leaving this
+// here as a simple (generic) example to write future tests
+#if 0
+WVTEST_MAIN("daemon quit")
+{
+    UniConfRoot cfg("temp:");
+    signal(SIGPIPE, SIG_IGN);
+
+    cfg["pickles"].setme("foo");
+    cfg["subtree/fries"].setme("bar1");
+    cfg["subtree/ketchup"].setme("bar2");
+    UniConfDaemon daemon(cfg, false, NULL);
+
+    WvStringList commands;
+    commands.append("quit");
+    WvStringListList expected_responses;
+    WvStringList hello_response;
+    hello_response.append("HELLO {UniConf Server ready.}");
+    expected_responses.add(&hello_response, false);
+    WvStringList expected_quit_response;
+    expected_quit_response.append("OK ");
+    expected_responses.add(&expected_quit_response, false);
+
+    WvString pipename("/tmp/tmpfile1");
+    WVPASS(daemon.setupunixsocket(pipename));
+    WvUnixAddr addr(pipename);
+    WvUnixConn *sock = new WvUnixConn(addr);
+    UniConfDaemonClientConn conn(sock, &commands, &expected_responses);
+
+    WvIStreamList::globallist.append(&conn, false);
+    WvIStreamList::globallist.append(&daemon, false);
+    printf("You are about to enter the no spin zone\n");
+    while (!WvIStreamList::globallist.isempty() && 
+           conn.isok() && daemon.isok())
+    {
+        printf("Spinning: streams left: %i\n", 
+               WvIStreamList::globallist.count());
+        WvIStreamList::globallist.runonce();
+    }
+
+    WVPASS(daemon.isok());
+    WvIStreamList::globallist.zap();
+}
+
+/**** Daemon proxying test ****/
+
+// test that proxying between two uniconf daemons works
+// e.g.: client -> uniconfd -> uniconfd 519-496-0675
+
+const char * const uniconfd1_args[] = {
+    "uniconf/daemon/uniconfd",
+    "-f",
+    "-p",
+    "0",
+    "-s"
+    "0",
+    "-u",
+    "/tmp/tmpfile1",
+    "/cfg=ini:/tmp/dumb.ini",
+    NULL
+};
+
+const char * const uniconfd1_2_args[] = {
+    "uniconf/daemon/uniconfd",
+    "-f",
+    "-p",
+    "0",
+    "-s"
+    "0",
+    "-u",
+    "/tmp/tmpfile1",
+    "/=temp:",
+    "/cfg=ini:/tmp/dumb.ini",
+    NULL
+};
+
+WvPipe * setup_master_daemon(bool implicit)
+{
+    WvFile stuff("/tmp/dumb.ini", O_WRONLY|O_TRUNC);
+    stuff.print("pickles/apples/foo=1\n");
+    stuff.print("pickles/mangos/bar=1\n");
+    stuff.close();
+
+    return new WvPipe(uniconfd1_args[0], 
+                      implicit ? uniconfd1_args : uniconfd1_2_args,
+                      false, true, false);
+}
+
+const char * const uniconfd2_args[] = {
+    "uniconf/daemon/uniconfd",
+    "-f",
+    "-p",
+    "0",
+    "-s"
+    "0",
+    "-u",
+    "/tmp/tmpfile2",
+    "/=retry:unix:/tmp/tmpfile1",
+    NULL
+};
+
+const char * const uniconfd2_2_args[] = {
+    "uniconf/daemon/uniconfd",
+    "-f",
+    "-p",
+    "0",
+    "-s"
+    "0",
+    "-u",
+    "/tmp/tmpfile2",
+    "/=retry:cache:unix:/tmp/tmpfile1",
+    NULL
+};
+
+void daemon_proxy_test(bool implicit)
+{
+    WvString pipename = "/tmp/tmpfile2";
+
+    WvPipe *master = setup_master_daemon(implicit);
+    master->setcallback(WvPipe::ignore_read, NULL);
+    master->nowrite();
+    fprintf(stderr, "Got here (1)\n");
+
+    WvPipe *slave = new WvPipe(uniconfd2_args[0], 
+                               implicit ? uniconfd2_args : uniconfd2_2_args, 
+                               false, false, false); 
+    slave->setcallback(WvPipe::ignore_read, NULL);
+    slave->nowrite();
+
+    fprintf(stderr, "Got here (2)\n");
+
+    UniConfRoot cfg("unix:/tmp/tmpfile1");
+    WVPASS(cfg["cfg/pickles/apples/foo"].getmeint() == 1);
+
+    WvStringList commands;
+    commands.append("get /cfg/pickles/apples/foo");
+    commands.append("subt /");
+    WvStringListList expected_responses;
+    WvStringList hello_response;
+    hello_response.append("HELLO {UniConf Server ready.}");
+    expected_responses.add(&hello_response, false);
+    WvStringList expected_get_response;
+    expected_get_response.append("ONEVAL cfg/pickles/apples/foo 1");
+    expected_responses.add(&expected_get_response, false);
+    WvStringList expected_subt_response;
+    expected_subt_response.append("VAL cfg {}");
+    expected_responses.add(&expected_subt_response, false);
+
+    fprintf(stderr, "Got here (3)\n");
+
+    WvUnixAddr addr(pipename);
+    WvUnixConn *sock = new WvUnixConn(addr);
+    UniConfDaemonClientConn conn(sock, &commands, &expected_responses);
+    
+    WvIStreamList::globallist.append(&conn, false);
+    WvIStreamList::globallist.append(master, false);
+    WvIStreamList::globallist.append(slave, false);
+
+    printf("Spinning: streams left: %i\n", 
+           WvIStreamList::globallist.count());
+    while (!WvIStreamList::globallist.isempty() && 
+            conn.isok())
+    {
+        printf("Spinning: streams left: %i\n", 
+               WvIStreamList::globallist.count());
+        WvIStreamList::globallist.runonce();
+    }
+
+    WvIStreamList::globallist.zap();
+    WVRELEASE(master);
+    WVRELEASE(slave);
+}
+
+WVTEST_MAIN("daemon proxying - with cache")
+{
+    signal(SIGPIPE, SIG_IGN);
+
+    daemon_proxy_test(false);
+}
+
+WVTEST_MAIN("daemon proxying - cfg the only mount")
+{
+    signal(SIGPIPE, SIG_IGN);
+    
+    daemon_proxy_test(true);
+}
+#endif
