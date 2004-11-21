a7209834b1b4feeba4f938fdbc511fb5ddae59bc
pzion <pzion@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2004-11-21 00:02:30 +0000

    r4_2: added ReconnectCallback to UniRetryGen that gives notification when the retry generator reconnects to the underlying generator
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r4_2@8197 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- uniconf/t/uniretrygen.t.cc
+++ uniconf/t/uniretrygen.t.cc
@@ -1,7 +1,7 @@
 #include "wvtest.h"
 #include "uniconfroot.h"
 #include "unitempgen.h"
-#include "unireplicategen.h"
+#include "uniretrygen.h"
 
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -71,5 +71,36 @@ WVTEST_MAIN("uniconfd")
     sleep(1); // Wait for UDS to go bad
 
     WVPASS(!cfg["/key"].exists());
+}
+
+bool reconnected = false;
+void reconnect_cb(UniRetryGen &uni)
+    { reconnected = true; }
+
+WVTEST_MAIN("reconnect callback")
+{
+    signal(SIGPIPE, SIG_IGN);
+
+    pid_t uniconfd_pid;
+    
+    unlink(UNICONFD_INI);
+    
+    UniConfRoot cfg;
+    cfg.mountgen(new UniRetryGen("unix:" UNICONFD_SOCK,
+                UniRetryGen::ReconnectCallback(reconnect_cb), 100));
+
+    reconnected = false;
+    unlink(UNICONFD_SOCK);
+    if ((uniconfd_pid = fork()) == 0)
+    {
+    	execv("uniconf/daemon/uniconfd", argv);
+    	_exit(1);
+    }
+    sleep(1); // Wait for reconnect
+    cfg.getme(); // Do something to reconnect
+    WVPASS(reconnected);
+
+    kill(uniconfd_pid, 15);
+    waitpid(uniconfd_pid, NULL, 0);
 }
 
