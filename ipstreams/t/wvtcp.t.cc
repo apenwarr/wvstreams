9b6adce4758a08fbe55f271c18b0e02dbf3d2355
andrew <andrew@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2005-02-04 04:01:31 +0000

    r4_1: Checking in wvtcp.t.cc unit test, which breaks with the fdstream patch, and works without it. BUGZID:10461
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/r4_1@9127 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- ipstreams/t/wvtcp.t.cc
+++ ipstreams/t/wvtcp.t.cc
@@ -1,87 +1,37 @@
-#include "wvtcp.h"
 #include "wvtest.h"
-#include <errno.h>
+#include "wvtcp.h"
+#include "wvistreamlist.h"
 
-WVTEST_MAIN("tcp connection")
+#define BASEPORT	11223
+
+static void lcallback(WvStream&, void *userdata)
 {
-    WvTCPListener listen("0.0.0.0:0");
-    WVPASS(listen.isok());
-    WvIPPortAddr port(*listen.src());
-    //wvcon->print("Local port is '%s'\n", port);
-
-    WvTCPConn tcp(port);
-    WVPASS(1);
-    tcp.write("foo\n");
-    WVPASS(2);
- 
-    listen.runonce(0);
-    WVPASS(3);
-    tcp.runonce(0);
-    WVPASS(4);
-    tcp.runonce(0);
-    WVPASS(5);
-    tcp.runonce(0);
-    tcp.runonce(0);
-    WVPASS(6);
-    listen.runonce(100);
-    WVPASS(7);
-    WvTCPConn *in = listen.accept();
-    WVPASS(8);
-    WVPASS(in);
-    if (!in) return; // forget it...
-    
-    WVPASS(in->isok());
-    
-    tcp.runonce(100);
-    tcp.runonce(100);
-    tcp.runonce(100);
-    
-    WVPASS(in->isreadable());
-    
-    WvString line = in->blocking_getline(100);
-    WVPASSEQ(line, "foo");
-    
-    in->release();
+    WvTCPListener *l = (WvTCPListener *)userdata;
+    WvTCPConn *conn = l->accept();
+    WvIStreamList::globallist.append(conn, true);
 }
 
-
-WVTEST_MAIN("connection refused")
+WVTEST_MAIN("tcp establish connection")
 {
-    char buf[1024];
-    size_t len;
-    
-    WvTCPListener listen("0.0.0.0:0");
-    WVPASS(listen.isok());
-    WvIPPortAddr port(*listen.src());
-//    WvIPPortAddr port("127.0.0.1:11223");
-//    WvIPPortAddr port("192.168.12.100:11223");
-    //wvcon->print("Local port is '%s'\n", port);
-    listen.close();
-    listen.runonce(0);
-    WVFAIL(listen.isok());
-    WVPASSEQ(listen.geterr(), 0);
-    
-    WvTCPConn tcp(port);
-    tcp.runonce(10000);
-    tcp.runonce(10000);
-    tcp.runonce(10000);
-    tcp.runonce(10000);
-    WVPASS(tcp.isconnected()); // even if error, we're past connection phase
-    WVFAIL(tcp.isok());
-    WVPASSEQ(tcp.geterr(), ECONNREFUSED);
-    printf("Error string is '%s'\n", tcp.errstr().cstr());
-//    WVPASSEQ(tcp.errstr(), strerror(ECONNREFUSED)); // wrong in win32
-    
-    len = tcp.read(buf, sizeof(buf));
-    WVPASSEQ(len, 0);
-    WVFAIL(tcp.isok());
-    WVPASSEQ(tcp.geterr(), ECONNREFUSED);
-    printf("Error string is '%s'\n", tcp.errstr().cstr());
-//    WVPASSEQ(tcp.errstr(), strerror(ECONNREFUSED)); // wrong in win32
-    
-    tcp.write("foo\n");
-    WVFAIL(tcp.isok());
-    WVPASSEQ(tcp.geterr(), ECONNREFUSED);
-    printf("Error string is '%s'\n", tcp.errstr().cstr());
-//    WVPASSEQ(tcp.errstr(), strerror(ECONNREFUSED)); // wrong in win32
+    int port = BASEPORT + getpid();
+
+    WvString laddrstr("0.0.0.0:%s", port);
+    WvIPPortAddr laddr(laddrstr);
+    WvTCPListener l(laddr);
+    l.setcallback(lcallback, &l);
+    WvIStreamList::globallist.append(&l, false);
+
+    printf("Starting listener on %s\n", laddrstr.cstr());
+
+    WVPASS(l.isok());
+
+    WvString caddrstr("127.0.0.1:%s", port);
+    WvIPPortAddr caddr(caddrstr);
+    WvTCPConn c(caddr);
+    WvIStreamList::globallist.append(&c, false);
+
+    WVPASS(c.isok());
+
+    WVPASS(c.select(0));
 }
+
