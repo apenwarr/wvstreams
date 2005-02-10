4edde3ae4fd62184843366f1cc4afff1bc79be5d
pzion <pzion@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2005-02-10 18:00:45 +0000

    HEAD: added set callbacks to UniCallbackGen
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@9197 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- uniconf/t/unicallbackgen.t.cc
+++ uniconf/t/unicallbackgen.t.cc
@@ -1,22 +1,90 @@
 #include "wvtest.h"
 #include "unicallbackgen.h"
 
-WvString generator(const UniConfKey &key)
+class AutoIncrementer
 {
-    static int i = 0;
-    return i++;
-}
+    int value;
+    
+public:
+    
+    AutoIncrementer() : value(0) {}
+    
+    WvString get(const UniConfKey &) { return value++; }
+    void set(const UniConfKey &, WvStringParm _value) { value = _value.num(); }
+};
 
-WVTEST_MAIN("even more basic")
+WVTEST_MAIN("non-dynamic")
 {
+    AutoIncrementer ai;
     UniCallbackGen g(1);
+
     WVPASS(!g.get("key"));
-    g.setvaluegencallback("key", UniCallbackGenCallback(generator));
-    WVPASS(g.get("key") == WvString(0));
+    
+    g.setgetcallback("key",
+            UniCallbackGenGetCallback(&ai, &AutoIncrementer::get));
+    WVPASS((g.set("key", "foo"), g.get("key")) == WvString(0));
     WVPASS((g.set("key", "foo"), g.get("key")) == WvString(1));
     WVPASS((g.set("key", "foo"), g.get("key")) == WvString(2));
     WVPASS((g.set("key", "foo"), g.get("key")) == WvString(3));
     WVPASS((g.set("key", "foo"), g.get("key")) == WvString(4));
-    g.setvaluegencallback("key", UniCallbackGenCallback());
+
+    g.setgetcallback("key", UniCallbackGenGetCallback());
+    WVPASS((g.set("key", "foo"), !g.get("key")));
+}
+
+WVTEST_MAIN("dynamic")
+{
+    AutoIncrementer ai;
+    UniCallbackGen g(1);
+
+    WVPASS(!g.get("key"));
+
+    g.update_after_set = false;
+    g.update_before_get = true;
+    
+    g.setgetcallback("key",
+            UniCallbackGenGetCallback(&ai, &AutoIncrementer::get));
+
+    WVPASS(g.get("key") == WvString(0));
+    WVPASS(g.get("key") == WvString(1));
+    WVPASS(g.get("key") == WvString(2));
+    WVPASS(g.get("key") == WvString(3));
+    WVPASS(g.get("key") == WvString(4));
+
+    g.setgetcallback("key", UniCallbackGenGetCallback());
     WVPASS(!g.get("key"));
 }
+
+WVTEST_MAIN("set")
+{
+    AutoIncrementer ai;
+    UniCallbackGen g(1);
+
+    WVPASS(!g.get("key"));
+
+    g.update_after_set = false;
+    g.update_before_get = true;
+    
+    g.setgetcallback("key",
+            UniCallbackGenGetCallback(&ai, &AutoIncrementer::get));
+    g.setsetcallback("key",
+            UniCallbackGenSetCallback(&ai, &AutoIncrementer::set));
+
+    WVPASS(g.get("key") == WvString(0));
+    WVPASS(g.get("key") == WvString(1));
+    WVPASS(g.get("key") == WvString(2));
+    WVPASS(g.get("key") == WvString(3));
+    WVPASS(g.get("key") == WvString(4));
+
+    g.set("key", 0);
+
+    WVPASS(g.get("key") == WvString(0));
+    WVPASS(g.get("key") == WvString(1));
+    WVPASS(g.get("key") == WvString(2));
+    WVPASS(g.get("key") == WvString(3));
+    WVPASS(g.get("key") == WvString(4));
+
+    g.setgetcallback("key", UniCallbackGenGetCallback());
+    WVPASS(!g.get("key"));
+}
+
