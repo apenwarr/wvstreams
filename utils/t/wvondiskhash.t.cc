6e027a3dad97a9c6c93c9d19e8fa9e7a40cd1cc6
jdeboer <jdeboer@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2005-03-17 20:05:17 +0000

    carambola: Nothing like fixing a problem twice. The WvOnDiskHash test was timing out on machines with high load and a large number of files in /tmp. To fix this problem, I added a 'ls | wc' at the beginning of the test. This should bring /tmp into cache for the *much* slower WvDirIter. Second, I added a WVPASS for every five hundredth file wvtest_filecountprefix checks, this resets the 40 second timer. BUGZID:11070
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@9392 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- utils/t/wvondiskhash.t.cc
+++ utils/t/wvondiskhash.t.cc
@@ -9,10 +9,25 @@ typedef WvOnDiskHash<int, int> IntMap;
 
 WVTEST_MAIN("dont create files in /tmp")
 {
-    WVPASS("tick 1");
+    WVPASS("start of test");
+    
+    // loads /tmp into cache to make things run consistantly fast.
+    time_t s = time(NULL);
+    system("ls /tmp |wc");
+    printf("ls /tmp: elasped %d\n", time(NULL) - s);
+    
+    WVPASS("after reading /tmp");
+    
+    s = time(NULL);
     int before_count = wvtest_file_count_prefix("/tmp", "file");
+    printf("before_count: elasped %d\n", time(NULL) - s);
+    
     WVPASS("tick 2");
+
+    s = time(NULL);
     int before_count_var = wvtest_file_count_prefix("/var/tmp", "qdbm-annoy");
+    printf("before_count_var: elasped %d\n", time(NULL) - s);
+    
     WVPASS("tick 3");
 
     IntMap *im = new IntMap;    
@@ -20,11 +35,18 @@ WVTEST_MAIN("dont create files in /tmp")
     delete im;
     WVPASS("tick 5");
 
+    s = time(NULL);
     int after_count = wvtest_file_count_prefix("/tmp", "file");
+    printf("after_count: elasped %d\n", time(NULL) - s);
+    
     WVPASS("tick 6");
+    
+    s = time(NULL);
     int after_count_var = wvtest_file_count_prefix("/var/tmp", "qdbm-annoy");
-    WVPASS(before_count == after_count);
-    WVPASS(before_count_var == after_count_var);
+    printf("after_count_var: elasped %d\n", time(NULL) - s);
+    
+    WVPASSEQ(before_count, after_count);
+    WVPASSEQ(before_count_var, after_count_var);
 
 }
 
