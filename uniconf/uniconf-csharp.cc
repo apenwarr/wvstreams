b10c593e34e4b0de5c5a2dd9e0d83d4317c60917
wlach <wlach@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2005-03-24 01:37:21 +0000

    HEAD: Commit fixed (but memory leaking!) uniconf-csharp uniconf key getting functions.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@9418 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- uniconf/uniconf-csharp.cc
+++ uniconf/uniconf-csharp.cc
@@ -65,13 +65,16 @@ uniconfkey_t uniconf_key(uniconf_t uni)
 const char * uniconf_key_str(uniconf_t uni)
 {
     UniConfKey r = ((UniConf*)uni)->key();
-    return r.cstr();
+    printf("DBG: %s\n", r.cstr());
+    WvString *foo = new WvString(r.cstr());
+    return foo->cstr();
 }
 
 const char * uniconf_fullkey_str(uniconf_t uni)
 {
     UniConfKey r = ((UniConf*)uni)->fullkey();
-    return r.cstr();
+    WvString *foo = new WvString(r.cstr());
+    return foo->cstr();
 }
 
 const uniconf_t uniconf_u(uniconf_t uni, const uniconfkey_t k) 
