698efc9192ac86713f32da6e2095b09a896ec845
pphaneuf <pphaneuf@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2005-05-17 00:57:38 +0000

    Just some cleaning up in the existing wvondiskhash.t.cc stuff, before I add further tests.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/carambola@9845 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- utils/t/wvondiskhash.t.cc
+++ utils/t/wvondiskhash.t.cc
@@ -5,8 +5,6 @@
 #include "wvtest_filecountprefix.h"
 
 
-typedef WvOnDiskHash<int, int> IntMap;
-
 WVTEST_MAIN("dont create files in /tmp")
 {
     WVPASS("start of test");
@@ -14,36 +12,37 @@ WVTEST_MAIN("dont create files in /tmp")
     // loads /tmp into cache to make things run consistantly fast.
     time_t s = time(NULL);
     system("ls /tmp |wc");
-    printf("ls /tmp: elasped %d\n", time(NULL) - s);
+    printf("ls /tmp: elasped %d\n", static_cast<int>(time(NULL) - s));
     
     WVPASS("after reading /tmp");
     
     s = time(NULL);
     int before_count = wvtest_file_count_prefix("/tmp", "file");
-    printf("before_count: elasped %d\n", time(NULL) - s);
+    printf("before_count: elasped %d\n", static_cast<int>(time(NULL) - s));
     
     WVPASS("tick 2");
 
     s = time(NULL);
     int before_count_var = wvtest_file_count_prefix("/var/tmp", "qdbm-annoy");
-    printf("before_count_var: elasped %d\n", time(NULL) - s);
+    printf("before_count_var: elasped %d\n", static_cast<int>(time(NULL) - s));
     
     WVPASS("tick 3");
 
-    IntMap *im = new IntMap;    
-    WVPASS("tick 4");
-    delete im;
+    {
+	WvOnDiskHash<int, int> im;
+	WVPASS("tick 4");
+    }
     WVPASS("tick 5");
 
     s = time(NULL);
     int after_count = wvtest_file_count_prefix("/tmp", "file");
-    printf("after_count: elasped %d\n", time(NULL) - s);
+    printf("after_count: elasped %d\n", static_cast<int>(time(NULL) - s));
     
     WVPASS("tick 6");
     
     s = time(NULL);
     int after_count_var = wvtest_file_count_prefix("/var/tmp", "qdbm-annoy");
-    printf("after_count_var: elasped %d\n", time(NULL) - s);
+    printf("after_count_var: elasped %d\n", static_cast<int>(time(NULL) - s));
     
     WVPASSEQ(before_count, after_count);
     WVPASSEQ(before_count_var, after_count_var);
