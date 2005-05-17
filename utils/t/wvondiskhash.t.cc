8b9e73729cb33711be2c0ccf6584ca8a1098b8a9
pphaneuf <pphaneuf@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2005-05-17 01:25:17 +0000

    carambola: combined Darin's unit test with mine. Still disabled, as bug 7300 isn't fixed yet. BUGZID:11929
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/carambola@9846 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- utils/t/wvondiskhash.t.cc
+++ utils/t/wvondiskhash.t.cc
@@ -49,3 +49,113 @@ WVTEST_MAIN("dont create files in /tmp")
 
 }
 
+
+template<class Backend>
+void itertest_check(WvOnDiskHash<int, int, Backend> &hash, bool *seen,
+		    size_t sizeof_seen, size_t &count)
+{
+    WvOnDiskHash<int, int, Backend>::Iter i(hash);
+
+    memset(seen, 0, sizeof(seen));
+    count = 0;
+
+    for (i.rewind(); i.next(); )
+    {
+	int key = i.key();
+	int value = *i;
+
+	if (key == 1 && value == 11)
+	{
+	    WVFAIL(seen[0]);
+	    seen[0] = true;
+	}
+	else if (key == 3 && value == 13)
+	{
+	    WVFAIL(seen[1]);
+	    seen[1] = true;
+	}
+	else if (key == 7 && value == 17)
+	{
+	    WVFAIL(seen[2]);
+	    seen[2] = true;
+	}
+	else
+	{
+	    WVFAIL("unknown key!");
+	}
+	++count;
+    }
+}
+
+
+template<class Backend>
+void itertest_new()
+{
+    WvOnDiskHash<int, int, Backend> hash;
+    WvOnDiskHash<int, int, Backend>::Iter i(hash);
+    bool seen[3];
+    size_t count;
+
+    WVPASS(hash.isok());
+
+    hash.add(1, 11);
+    hash.add(3, 13);
+    hash.add(7, 17);
+
+    WVPASS(hash.isok());
+    WVPASS(hash.exists(1));
+    WVPASS(hash.exists(3));
+    WVPASS(hash.exists(7));
+    WVFAIL(hash.exists(42));
+
+    itertest_check(hash, seen, sizeof(seen), count);
+
+    WVPASSEQ(count, 3);
+    WVPASSEQ(hash.count(), 3);
+    WVPASS(seen[0]);
+    WVPASS(seen[1]);
+    WVPASS(seen[2]);
+
+    hash.remove(3);
+    WVPASS(hash.exists(1));
+    WVFAIL(hash.exists(3));
+    WVPASS(hash.exists(7));
+
+    itertest_check(hash, seen, sizeof(seen), count);
+
+    WVPASSEQ(count, 2);
+    WVPASSEQ(hash.count(), 2);
+    WVPASS(seen[0]);
+    WVFAIL(seen[1]);
+    WVPASS(seen[2]);
+
+    WVPASS(hash[1] == 11);
+    WVPASS(hash.find(7) == 17);
+
+    hash.zap();
+
+    itertest_check(hash, seen, sizeof(seen), count);
+
+    WVPASSEQ(count, 0);
+    WVPASS(hash.isempty());
+    WVPASSEQ(hash.count(), 0);
+    WVFAIL(seen[0]);
+    WVFAIL(seen[1]);
+    WVFAIL(seen[2]);
+}
+
+
+#if 0
+// FIXME: there's an evil leak, bug 7300
+WVTEST_MAIN("WvOnDiskHash with WvQdbmHash backend")
+{
+    itertest_new<WvQdbmHash>();
+}
+#endif
+
+
+WVTEST_MAIN("WvOnDiskHash with WvBdbHash backend")
+{
+    itertest_new<WvBdbHash>();
+}
+
