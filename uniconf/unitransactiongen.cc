a8a06c0146ada6da0a7083d084bb69c327a2436c
pzion <pzion@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2005-07-21 19:50:59 +0000

    Merged revisions 10146-10235 via svnmerge from /branches/carambola
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@10238 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- uniconf/unitransactiongen.cc
+++ uniconf/unitransactiongen.cc
@@ -264,14 +264,12 @@ void UniTransactionGen::commit()
 {
     if (root)
     {
-	// We ignore callbacks during commit() so that we don't waste
-	// time in gencallback() for every set() we make during
-	// apply_changes().
-	base->del_callback(this);
+	// Apply our changes to the inner generator.  We can't optimise
+	// away callbacks at this point, because we may get notified of
+	// changes caused by our changes.
+	hold_delta();
 	apply_changes(root, UniConfKey());
-	base->add_callback(this,
-	    UniConfGenCallback(this, &UniTransactionGen::gencallback), NULL);
-	
+
 	// make sure the inner generator also commits
 	base->commit();
 
@@ -279,6 +277,7 @@ void UniTransactionGen::commit()
 	// redundant notifications caused by the base->commit()
 	delete root;
 	root = NULL;
+	unhold_delta();
     }
     
     // no need to base->commit() if we know we haven't changed anything!
