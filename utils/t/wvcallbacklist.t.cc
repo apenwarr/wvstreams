db3e4707606d5195742f58b65943853c0017f262
pphaneuf <pphaneuf@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2005-08-06 04:47:55 +0000

    Added a WvCallbackList, and an accompanying unit test.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@10311 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ utils/t/wvcallbacklist.t.cc
@@ -0,0 +1,47 @@
+#include "wvcallbacklist.h"
+#include "wvtest.h"
+#include "wvcallback.h"
+
+
+typedef WvCallback<int, int&> TestCallback;
+
+
+/* The return value will be ignored. */
+int testcb(int &count)
+{
+    ++count;
+    return 42;
+}
+
+
+WVTEST_MAIN("WvCallbackList sanity")
+{
+    WvCallbackList<TestCallback> list;
+    const unsigned int numcb = 42;
+    char jar[numcb];
+    int count;
+
+    for (unsigned int i = 0; i < numcb; ++i)
+	list.add(testcb, &jar[i]);
+
+    count = 0;
+    list(count);
+
+    WVPASSEQ(count, numcb);
+
+    list.del(&jar[0]);
+
+    count = 0;
+    list(count);
+
+    WVPASSEQ(count, numcb - 1);
+
+    for (unsigned int i = 1; i < numcb; ++i)
+	list.del(&jar[i]);
+
+    count = 0;
+    list(count);
+
+    WVPASSEQ(count, 0);
+}
+
