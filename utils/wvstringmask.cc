6e897b223325b86eb0674d6c30b88edc77f85d2d
sfllaw <sfllaw@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2005-08-11 21:28:26 +0000

    Wrote WvStringMask, which is a set of characters, which you can ask if a character is included or not.  This is much faster than strchr().
    BUGZID: 14057
    
    
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@10361 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ utils/wvstringmask.cc
@@ -0,0 +1,68 @@
+/* -*- Mode: C++ -*-
+ * Worldvisions Weaver Software:
+ *   Copyright (C) 2005 Net Integration Technologies, Inc.
+ *
+ * Implementation of an efficient lookup for a set characters.
+ *
+ * It is, however, a little space intensive, but you should statically
+ * create them in your functions, and then they won't be so bad.
+ */
+#include "wvstringmask.h"
+
+WvStringMask::WvStringMask(WvStringParm s)
+{
+    zap();
+    set(s, true);
+}
+
+WvStringMask::WvStringMask(char c)
+{
+    zap();
+    set(c, true);
+}
+
+bool WvStringMask::operator[](const char c) const
+{
+    return _set[unsigned(c)];
+}
+
+bool WvStringMask::operator[](const int i) const
+{
+    return _set[int(i)];
+}
+
+const char WvStringMask::first() const
+{
+    return _first;
+}
+
+void WvStringMask::zap()
+{
+    memset(_set, 0, sizeof(bool) * sizeof(_set));
+    _first = '\0';
+}
+
+void WvStringMask::set(const char c, bool value)
+{
+    if (!_first)
+	_first = c;
+
+    _set[unsigned(c)] = value;
+}
+
+void WvStringMask::set(WvStringParm s, bool value)
+{
+    if (!s.isnull())
+    {
+	const char *c = s.cstr();
+
+	if (!_first)
+	    _first = *c;
+
+	while (*c)
+	{
+	    _set[unsigned(*c)] = value;
+	    ++c;
+	}
+    }
+}
