c0363794312b9e52deb8b904cd15e1db9acab27f
pzion <pzion@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2006-03-16 18:36:48 +0000

    WvStreams: added WvPagePool, an on-disk mmap-based page allocator
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/lime@11039 6619c942-59fa-0310-bf43-a07aaa81d9ac

---
+++ utils/t/wvpagepool.t.cc
@@ -0,0 +1,63 @@
+#include "wvtest.h"
+#include "wvpagepool.h"
+#include "wvfileutils.h"
+#include "wvstream.h"
+#include "wvfile.h"
+
+static void dump_maps()
+{
+    WvFile f("/proc/self/maps", O_RDONLY);
+    while (f.isok())
+    {
+        const char *line = f.blocking_getline(-1);
+        if (line == NULL)
+            break;
+        wvout->print("%s\n", line);
+    }
+}
+
+
+WVTEST_MAIN("on-disk")
+{
+    int i;
+    WvString filename = wvtmpfilename("on-disk");
+    WvPagePool pp(filename, O_RDWR | O_CREAT | O_TRUNC);
+    const int count = 16, max_num_pages = 64, iterations = 4096;
+    WvPagePool::page_id_t page_id[count];
+    int num_pages[count];
+
+    WVPASS(pp.isok());
+    
+    for (i=0; i<count; ++i)
+    {
+        page_id[i] = 0;
+        num_pages[i] = 0;
+    }
+    
+    for (i=0; i<iterations; ++i)
+    {
+        int j = rand() % count;
+        
+        if (page_id[j] == 0)
+        {
+            num_pages[j] = rand() % max_num_pages + 1;
+            wverr->print("%s: alloc %s\n", i, num_pages[j]);
+            page_id[j] = pp.alloc(num_pages[j]);
+            WVPASS(pp.addr(page_id[j]) != NULL);
+            //dump_maps();
+            memset(pp.addr(page_id[j]), rand()%256, pp.size(num_pages[j]));
+        }
+        else
+        {
+            wverr->print("%s: free %s, %s\n", i, page_id[j], num_pages[j]);
+            memset(pp.addr(page_id[j]), rand()%256, pp.size(num_pages[j]));
+            pp.free(page_id[j], num_pages[j]);
+            //dump_maps();
+            page_id[j] = 0;
+        }
+    }
+    
+    //unlink(filename);
+}
+
+
