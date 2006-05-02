bb54befc105e3e453a26100f89e8166d539edfe0
pphaneuf <pphaneuf@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2006-05-02 22:20:59 +0000

    WvStreamClone's post_select hides some of the results of the cloned stream, but pre_select wasn't, so it would trip the assert in WvIStreamList::post_select, even though the cloned stream would actually behave correctly. This hiding is for reasons internal to WvStreamClone and is fine, so we "completed" the hiding in pre_select as well, by deferring the work to post_select. BUGZID:19999
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/branches/idb2@11171 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- streams/wvstreamclone.cc
+++ streams/wvstreamclone.cc
@@ -209,12 +209,21 @@ bool WvStreamClone::pre_select(SelectInfo &si)
 
 	pre_select_result = cloned->pre_select(si);
 	assert(!pre_select_result || si.msec_timeout == 0);
-	result = result || pre_select_result;
+
+	// At this point, one would be tempted to combine
+	// "pre_select_result" with "result" (that's what we used to
+	// do, anyway). Don't. The problem is that if we are trying to
+	// flush our outbuf or autoclose, we'll have twiddled the
+	// interest set, and post_select will "eat" that result. So we
+	// have to "eat" it as well, to prevent the assert in
+	// WvIStreamList::post_select from tripping. But it's okay,
+	// we'll just set the msec_timeout to zero and take care of
+	// things in our post_select...
 	
 	si.wants = oldwant;
     }
 
-    if (result)
+    if (result || pre_select_result)
 	si.msec_timeout = 0;
 
     return result;
