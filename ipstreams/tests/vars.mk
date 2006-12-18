aa39930279f51ce42fab87ee39b9ec52dc6b7020
wlach <wlach@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2006-12-18 23:46:50 +0000

    Let user run unit tests even if they don't have the readline library installed. You don't need the wsd executable to do so.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/releases/freesw@11468 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- ipstreams/tests/vars.mk
+++ ipstreams/tests/vars.mk
@@ -1,5 +1,8 @@
 
-TESTS+=$(patsubst %.cc,%,$(wildcard ipstreams/tests/*.cc))
+TESTS+=$(filter-out ipstreams/tests/wsd, $(patsubst %.cc,%,$(wildcard ipstreams/tests/*.cc)))
+ifneq ("$(with_readline)", "no")
+TESTS+=ipstreams/tests/wsd
+endif
 
 ipstreams/tests/xplctest: LIBS+=-lxplc-cxx -lxplc
 ipstreams/tests/wsd: LIBS+=-lxplc-cxx -lxplc -lreadline
